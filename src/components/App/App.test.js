e62b52ea263d2bca82f615357ec4d6de41ef59b0
Federico <federico.obialero@gmail.com>
2018-03-19 17:44:17 +0100

    update test scripts

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import renderer from 'react-test-renderer'
+import App from './App.jsx'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
   ReactDOM.unmountComponentAtNode(div)
+})
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+      <div>
+        <h1>RigoBlock</h1>
+      </div>
+    )
+    .toJSON()
+  expect(tree).toMatchSnapshot()
 })
