5802c770ee6cfe7ee38dff866365ce9729cb58d8
Federico <federico.obialero@gmail.com>
2018-03-20 12:11:35 +0100

    minor fix according to review

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App-wrapper">
+      <div className="app-wrapper">
         <h1>RigoBlock</h1>
       </div>
     )
