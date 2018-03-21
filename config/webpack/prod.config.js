17962adc88cf8da93f5e33684b743815c70f51f5
Federico <federico.obialero@gmail.com>
2018-03-21 14:06:30 +0100

    changes to webpack production config

--- config/webpack/prod.config.js
+++ config/webpack/prod.config.js
@@ -33,7 +33,7 @@ if (env.stringified['process.env'].NODE_ENV !== '"production"') {
 }
 
 // Note: defined here because it will be used more than once.
-const cssFilename = 'static/css/[name].[contenthash:8].css'
+const cssFilename = 'static/css/[hash].css'
 
 // ExtractTextPlugin expects the build output to be flat.
 // (See https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/27)
@@ -62,13 +62,25 @@ module.exports = {
     // Generated JS file names (with nested folders).
     // There will be one main bundle, and one file per asynchronous chunk.
     // We don't currently advertise code splitting but Webpack supports it.
-    filename: 'static/js/[name].[chunkhash:8].js',
-    chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
+    filename: 'static/js/[id].[hash].bundle.js',
     // We inferred the "public path" (such as / or /my-project) from homepage.
     publicPath: publicPath,
     // Point sourcemap entries to original disk location (format as URL on Windows)
     devtoolModuleFilenameTemplate: info =>
       path.relative(paths.appSrc, info.absoluteResourcePath).replace(/\\/g, '/')
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          chunks: 'initial',
+          name: 'vendor',
+          filename: 'static/js/[id].[hash].vendor.bundle.js',
+          enforce: true
+        }
+      }
+    }
   },
   resolve: {
     // This allows you to set a fallback for where Webpack should look for modules.
@@ -118,7 +130,7 @@ module.exports = {
             loader: require.resolve('url-loader'),
             options: {
               limit: 10240,
-              name: 'static/media/[name].[hash:8].[ext]'
+              name: 'static/media/[hash].[ext]'
             }
           },
           // Process JS with Babel.
@@ -204,7 +216,7 @@ module.exports = {
             // by webpacks internal loaders.
             exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
             options: {
-              name: 'static/media/[name].[hash:8].[ext]'
+              name: 'static/media/[hash].[ext]'
             }
           }
           // ** STOP ** Are you adding a new loader?
