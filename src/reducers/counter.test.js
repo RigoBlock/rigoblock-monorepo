93e07768873f07adf820fc135f5cee2e89f30c94
Federico <federico.obialero@gmail.com>
2018-03-26 15:33:25 +0200

    add deep-freeze to test if reducers are pure functions

--- src/reducers/counter.test.js
+++ src/reducers/counter.test.js
@@ -1,13 +1,28 @@
 import counter from './counter'
 import actions from '../actions/counter-actions'
+import deepFreeze from 'deep-freeze'
+
+const reducerTester = function(reducer) {
+  return function(currentState, action, expectedState) {
+    if (currentState && typeof currentState === 'object') {
+      deepFreeze(currentState)
+    }
+    const newState = reducer(currentState, action)
+    return expect(newState).toEqual(expectedState)
+  }
+}
+
+window.reducerTester = reducerTester
 
 describe('counter reducer', () => {
+  const counterReducer = reducerTester(counter)
+
   it('should return the initial state', () => {
-    expect(counter(undefined, {})).toEqual(0)
+    counterReducer(undefined, {}, 0)
   })
 
   it('should add 1 to the state when the add action is invoked', () => {
-    expect(counter(undefined, actions.add())).toEqual(1)
-    expect(counter(1, actions.add())).toEqual(2)
+    counterReducer(0, actions.add(), 1)
+    counterReducer(1, actions.add(), 2)
   })
 })
