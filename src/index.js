f085f55672c155ffc68839ae9ecae0800c128d40
Federico <SlyFox42@users.noreply.github.com>
2018-03-26 12:37:09 +0200

    Add Yarn dev script and hot reload (#23)
    * add yarn script to start dev server
    
    * add hot reload

--- src/index.js
+++ src/index.js
@@ -7,3 +7,7 @@ import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
+
+if (module.hot) {
+  module.hot.accept()
+}
