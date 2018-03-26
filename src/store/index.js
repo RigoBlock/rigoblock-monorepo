0e78acb1ca2b265007bf8a5e2b37b10d1d15e454
Federico <federico.obialero@gmail.com>
2018-03-26 18:43:04 +0200

    add reactotron support for development

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,22 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from '../reducers/index'
 
 export default (initialState = {}) => {
-  const storeEnhancer =
-    typeof window !== 'undefined' && window.devToolsExtension
-      ? window.devToolsExtension()
-      : f => f
-  const store = createStore(rootReducer, initialState, storeEnhancer)
-  return store
+  let middlewares = []
+  let storeCreator = createStore
+
+  if (window !== null && window.devToolsExtension) {
+    middlewares.push(window.devToolsExtension())
+  }
+
+  if (process.env.NODE_ENV !== 'production') {
+    const Reactotron = require('reactotron-react-js').default
+    const { reactotronRedux } = require('reactotron-redux')
+    Reactotron.configure()
+      .use(reactotronRedux())
+      .connect()
+    storeCreator = Reactotron.createStore
+  }
+
+  return storeCreator(rootReducer, initialState, compose(...middlewares))
 }
