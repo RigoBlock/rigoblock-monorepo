ee934e64c79026995352c597b69e570f709e4df5
Federico <federico.obialero@gmail.com>
2018-03-28 12:42:26 +0200

    add test second counter

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { routerReducer as routing } from 'react-router-redux'
 import counter from './counter'
+import secondCounter from './secondCounter'
 
 export default combineReducers({
   counter,
+  secondCounter,
   routing
 })
---
+++ src/reducers/secondCounter.js
@@ -0,0 +1,16 @@
+import persistentDecorator from '../store/persistentDecorator'
+import { actionTypes } from '../constants/action-types'
+
+function counter(state = { count: 0 }, action) {
+  switch (action.type) {
+    case actionTypes.COUNTER_ADD:
+      return {
+        ...state,
+        count: state.count + action.amount
+      }
+    default:
+      return state
+  }
+}
+
+export default persistentDecorator(counter, 'secondCounter')
