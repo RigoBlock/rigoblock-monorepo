dd9deb2a54739543ea4432f5d6f1361f1c2d0442
Luca Del Bianco <vshjxyz@gmail.com>
2018-04-06 15:52:12 +0200

    Removing seed from ganache

--- packages/ganache-bootstrap/bootstrap.js
+++ packages/ganache-bootstrap/bootstrap.js
@@ -2,12 +2,10 @@ const Web3 = require('web3')
 const c = require('chalk')
 const { deploy } = require('@rigoblock/protocol')
 const logger = require('./logger')
-const protocolSeed = require('./protocolSeed')
 
 module.exports = async network => {
   const web3 = new Web3(new Web3.providers.HttpProvider(network))
   const accList = await web3.eth.getAccounts()
   logger.info('BASE ACCOUNT', c.bold.magenta(accList[0]))
   const contracts = await deploy(accList[0], network)
-  await protocolSeed(accList, contracts)
 }
