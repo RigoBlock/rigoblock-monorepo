e71fb6487983bcc7a844a655a5ad654a4ee1ff33
Federico <federico.obialero@gmail.com>
2018-04-13 15:35:02 +0200

    add left-navbar organism (no molecules/atoms inside atm)

---
+++ packages/dapp/src/components/organisms/LeftNavbar/LeftNavbar.stories.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import LeftNavbar from './LeftNavbar'
+import '../../_settings/_base.scss'
+
+storiesOf('Organisms/LeftNavbar', module).add('default', () => <LeftNavbar />)
