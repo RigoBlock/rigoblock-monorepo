e8707c677bb420a7cef246f32925883bef213318
Luca Del Bianco <vshjxyz@gmail.com>
2018-04-13 19:10:24 +0200

    Add delay operator

--- packages/dapp/src/epics/counter.js
+++ packages/dapp/src/epics/counter.js
@@ -1,6 +1,7 @@
 import { Scheduler } from 'rxjs/Scheduler'
 import { actionTypes } from '../constants/action-types'
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/delay'
 
 export const counterEpic = (action$, store, ts = Scheduler.async) => {
   return action$
