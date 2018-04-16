2f7a206d2355b4338b181dd44c2617285b9b3744
Federico <federico.obialero@gmail.com>
2018-04-16 12:31:12 +0200

    created first login page

--- packages/dapp/src/pages/Login/Login.jsx
+++ packages/dapp/src/pages/Login/Login.jsx
@@ -1,18 +1,32 @@
 import React, { Component } from 'react'
 import BaseTemplate from '../../components/templates/BaseTemplate'
+import Link, { LINK_SIZES } from '../../components/atoms/Link'
 import './Login.scss'
 
 class Login extends Component {
   render() {
     return (
-      <div className="Login">
+      <div>
         <BaseTemplate>
-          <div className="login">
+          <div className="login-content">
             <h1>Hello there!</h1>
             <p>
               Rigoblock is a decentralized app that runs on Ethereum Blockchain.
-              To start using it, just follow these 3 simple steps
+              To start using it, just follow these 3 simple steps:
             </p>
+            <h3>1. Install a decentralized wallet</h3>
+            <p>We suggest to use Metamask or Parity.</p>
+            <a href={'https://metamask.io/'}>Metamask</a>
+            <a href={'https://www.parity.io/'}>Parity UI</a>
+            <h3>2. Transfer some funds to your decentralized wallet</h3>
+            <p>As you would do with a standard ETH wallet.</p>
+            <h3>3. Start investing with RigoBlock</h3>
+            <p>Now you are ready to deposit and invest on RigoBlock!</p>
+            <hr />
+            <h3>Need a hand?</h3>
+            <Link to={'#'} size={LINK_SIZES.SMALL}>
+              We are here to help!
+            </Link>
           </div>
         </BaseTemplate>
       </div>
