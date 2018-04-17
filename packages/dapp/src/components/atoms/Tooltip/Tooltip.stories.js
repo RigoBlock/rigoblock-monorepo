fbf0e72ddc8a07f6e2b4a1d4ff418dbde212278a
Federico <federico.obialero@gmail.com>
2018-04-17 17:49:43 +0200

    minor fix

--- packages/dapp/src/components/atoms/Tooltip/Tooltip.stories.js
+++ packages/dapp/src/components/atoms/Tooltip/Tooltip.stories.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { MemoryRouter } from 'react-router-dom'
 import '../../_settings/_base.scss'
 import Tooltip, { TOOLTIP_SIZES } from './Tooltip'
 
 storiesOf('Atoms/Tooltip', module)
-  .addDecorator(story => <MemoryRouter>{story()}</MemoryRouter>)
   .add(TOOLTIP_SIZES.SMALL, () => (
     <Tooltip size={TOOLTIP_SIZES.SMALL} type={'help'} to={'/'}>
       Small Tooltip
