8ab43d2eeb8f6e1f36c83277a1a27cd91bfe3f30
Federico <federico.obialero@gmail.com>
2018-04-24 18:03:08 +0200

    removed globl promise

--- packages/protocol/deployment/constants.js
+++ packages/protocol/deployment/constants.js
@@ -4,5 +4,24 @@ module.exports = {
   ARTIFACTS_DIR: path.resolve(__dirname, '../artifacts'),
   CONTRACTS_DIR: path.resolve(__dirname, '../contracts'),
   NETWORKS: process.env.NETWORKS || ['http://localhost:8545'],
-  GAS_ESTIMATE: 5e6
+  GAS_ESTIMATE: 5e6,
+  CONTRACT_NAMES: [
+    'Authority',
+    'Distribution',
+    'Drago',
+    'DragoEventful',
+    'DragoFactory',
+    'DragoRegistry',
+    'ERC20',
+    'Inflation',
+    'Migrations',
+    'Owned',
+    'OwnedUninitialized',
+    'ProofOfPerformance',
+    'RigoToken',
+    'UnlimitedAllowanceToken',
+    'Vault',
+    'VaultEventful',
+    'VaultFactory'
+  ]
 }
