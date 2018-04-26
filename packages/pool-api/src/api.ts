45fd506060a90b0f259fbcd23c8cc7cd449007ae
Federico <federico.obialero@gmail.com>
2018-04-26 17:35:10 +0200

    added types

--- packages/pool-api/src/api.ts
+++ packages/pool-api/src/api.ts
@@ -2,7 +2,7 @@ import * as ProviderEngine from 'web3-provider-engine'
 import { InjectedWeb3Subprovider } from '@0xproject/subproviders'
 import * as RpcSubprovider from 'web3-provider-engine/subproviders/rpc.js'
 import contracts from '@rigoblock/protocol/contracts'
-import Contract from './pools/contracts'
+import Contract from './pools'
 class PoolsApi {
   _contract: Contract
 
@@ -16,8 +16,8 @@ class PoolsApi {
     )
     engine.addProvider(new InjectedWeb3Subprovider((<any>window).web3.currentProvider))
     const web3 = engine._providers[1]._injectedWeb3
-    const networkId: Number = web3.version.network
-    const contractsMap = await contracts(networkId)
+    const networkId: number = web3.version.network
+    const contractsMap: object = await contracts(networkId)
     this._contract = await new Contract(web3, contractsMap)
   }
 
--- packages/pool-api/src/pools/index.ts
+++ packages/pool-api/src/pools/index.ts
@@ -1,1 +1,24 @@
-export { default } from './contracts'
+class Contract {
+  constructor(web3: object, contractsMap: object) {
+    if (!web3) {
+      throw new Error('web3 instance needs to be provided to Contract')
+    }
+    const deployedContracts: Array<string> = Object.keys(contractsMap).filter(
+      contractName => contractsMap[contractName].address
+    )
+
+    deployedContracts.forEach(async contractName => {
+      const contract: object = await import(`./contracts/${contractName}`)
+      this[`_${contractName}`] = new contract[contractName](
+        web3,
+        contractsMap[contractName].address
+      )
+
+      Object.defineProperty(this, contractName, {
+        get: () => this[`_${contractName}`].rawWeb3Contract
+      })
+    })
+  }
+}
+
+export default Contract
