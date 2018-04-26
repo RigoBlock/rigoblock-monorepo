0c4d0364dffe7bac7647903c79007941a845325d
Federico <federico.obialero@gmail.com>
2018-04-26 12:02:43 +0200

    minor fixes

--- packages/protocol/contracts/index.js
+++ packages/protocol/contracts/index.js
@@ -1,1 +1,23 @@
-module.exports = require('./contracts')
+const { CONTRACT_NAMES } = require('../constants')
+
+module.exports = async networkId => {
+  const artifacts = CONTRACT_NAMES.map(contractName => `${contractName}.json`)
+  const abisPromises = artifacts.map(async artifact => {
+    const json = await import('../artifacts/' + artifact)
+    return json['networks'][networkId]
+      ? {
+          [artifact
+            .split('/')
+            .pop()
+            .replace('.json', '')]: {
+            abi: json['networks'][networkId].abi,
+            address: json['networks'][networkId].address
+          }
+        }
+      : console.error(
+          `Make sure contracts are deployed for network Id: ${networkId}`
+        )
+  })
+  const abisMap = await Promise.all(abisPromises)
+  return abisMap.reduce((acc, curr) => ({ ...acc, ...curr }), {})
+}
