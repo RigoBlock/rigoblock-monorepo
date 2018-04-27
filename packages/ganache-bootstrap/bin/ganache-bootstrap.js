4e3ca7153a7c76e131f4288434a4331ad6b78eb7
Luca Del Bianco <vshjxyz@gmail.com>
2018-04-27 12:45:36 +0200

    Fixing erorr codes to ensure circleci fails during compile, deploy and seed

--- packages/ganache-bootstrap/bin/ganache-bootstrap.js
+++ packages/ganache-bootstrap/bin/ganache-bootstrap.js
@@ -1,5 +1,10 @@
 #! /usr/bin/env node
+const c = require('chalk')
 const bootstrap = require('../bootstrap')
 const { GANACHE_URL } = require('../constants')
+const logger = require('./logger')
 
-bootstrap(GANACHE_URL).catch(e => console.error('Error', e))
+bootstrap(GANACHE_URL).catch(e => {
+  logger.error(c.red(`Error during bootstrap: ${e.stack}`))
+  process.exit(1)
+})
--- packages/ganache-bootstrap/bin/ganache-seed.js
+++ packages/ganache-bootstrap/bin/ganache-seed.js
@@ -1,5 +1,10 @@
 #! /usr/bin/env node
+const c = require('chalk')
 const seed = require('../seed')
 const { GANACHE_URL } = require('../constants')
+const logger = require('./logger')
 
-seed(GANACHE_URL).catch(e => console.error('Error', e))
+seed(GANACHE_URL).catch(e => {
+  logger.error(c.red(`Error during seed: ${e.stack}`))
+  process.exit(1)
+})
