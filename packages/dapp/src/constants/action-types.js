ec07c880e6fecd0c1c89f0a839b3c883ff951f6a
Federico <federico.obialero@gmail.com>
2018-05-08 16:11:46 +0200

    minor fix

--- packages/dapp/src/constants/action-types.js
+++ packages/dapp/src/constants/action-types.js
@@ -3,6 +3,7 @@ import keyMirror from 'keymirror'
 export const actionTypes = keyMirror({
   // Global actions
   GLOBAL_INIT: null,
+  // Blockchain actions
   BLOCKCHAIN_INIT: null,
   BLOCKCHAIN_ERROR: null,
   LOGGED_IN: null,
