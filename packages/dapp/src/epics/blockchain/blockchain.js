fac52c4b77a0667586a75f141ea6aee1cbb8dc67
Federico <federico.obialero@gmail.com>
2018-05-09 16:08:34 +0200

    blockchain epic now checks if metamask is installed

--- packages/dapp/src/epics/blockchain/blockchain.js
+++ packages/dapp/src/epics/blockchain/blockchain.js
@@ -1,9 +1,13 @@
+import 'rxjs/add/operator/delay'
 import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/mapTo'
 import 'rxjs/add/operator/merge'
 import 'rxjs/add/operator/mergeMap'
 import { Scheduler } from 'rxjs/Scheduler'
 import { Subject } from 'rxjs/Subject'
 import { actionTypes } from '../../constants/action-types'
+import { of } from 'rxjs/observable/of'
+import { push } from 'react-router-redux'
 import BlockChainService from './BlockChainService'
 import api from '../../api'
 
@@ -15,11 +19,11 @@ export const blockchainEpic = (action$, store, ts = Scheduler.async) => {
     blockchainSubject,
     ts
   )
-
-  return action$
-    .ofType(actionTypes.GLOBAL_INIT)
-    .mergeMap(() => blockchainService.init())
-    .merge(blockchainSubject)
+  return action$.ofType(actionTypes.GLOBAL_INIT).mergeMap(() => {
+    return window.web3
+      ? blockchainService.init().merge(blockchainSubject)
+      : of(push('/login'))
+  })
 }
 
 export default [blockchainEpic]
--- packages/dapp/src/epics/blockchain/blockchain.test.js
+++ packages/dapp/src/epics/blockchain/blockchain.test.js
@@ -14,8 +14,20 @@ jest.doMock('./BlockChainService', () => BlockChainServiceMock)
 jest.doMock('../../api', () => ({}))
 const blockchainEpic = require('./blockchain').blockchainEpic
 
+const pushAction = () => ({
+  type: '@@router/CALL_HISTORY_METHOD',
+  payload: {
+    args: ['/login'],
+    method: 'push'
+  }
+})
+
 describe('blockchainEpic', () => {
+  beforeEach(() => {
+    global.web3 = undefined
+  })
   it('dispatches an init action when the BlockChainService is initialized', () => {
+    global.web3 = {}
     const inputValues = {
       a: globalActions.init()
     }
@@ -24,6 +36,29 @@ describe('blockchainEpic', () => {
     }
 
     const inputMarble = 'a|'
+    const expectedMarble = 'b'
+
+    const ts = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected)
+    })
+
+    const action$ = new ActionsObservable(
+      ts.createHotObservable(inputMarble, inputValues)
+    )
+    const outputAction = blockchainEpic(action$, null, ts)
+
+    ts.expectObservable(outputAction).toBe(expectedMarble, expectedValues)
+    ts.flush()
+  })
+  it('dispatches a router push action if web3 is not defined', () => {
+    const inputValues = {
+      a: globalActions.init()
+    }
+    const expectedValues = {
+      b: pushAction()
+    }
+
+    const inputMarble = 'a'
     const expectedMarble = 'b'
 
     const ts = new TestScheduler((actual, expected) => {
