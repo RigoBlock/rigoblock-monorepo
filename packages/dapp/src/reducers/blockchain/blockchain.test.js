6be901457bd39a8ca2785e2eefea39615433f688
Federico <federico.obialero@gmail.com>
2018-05-31 15:48:49 +0200

    added new test to check that account isnt overwritten upon logging in again

--- packages/dapp/src/reducers/blockchain/blockchain.test.js
+++ packages/dapp/src/reducers/blockchain/blockchain.test.js
@@ -21,7 +21,30 @@ describe('blockchain reducer', () => {
         'Metamask/4.6.1',
         '0x242B2Dd21e7E1a2b2516d0A3a06b58e2D9BF9196'
       ),
-      { accounts: { ['0x242B2Dd21e7E1a2b2516d0A3a06b58e2D9BF9196']: {} } }
+      { accounts: { '0x242B2Dd21e7E1a2b2516d0A3a06b58e2D9BF9196': {} } }
+    )
+  })
+
+  it("doesn't overwrite an account if we log in again with it", () => {
+    blockChainTest(
+      {
+        accounts: {
+          '0x242B2Dd21e7E1a2b2516d0A3a06b58e2D9BF9196': {
+            dummyProp: 'dummyValue'
+          }
+        }
+      },
+      actions.blockChainLogIn(
+        'Metamask/4.6.1',
+        '0x242B2Dd21e7E1a2b2516d0A3a06b58e2D9BF9196'
+      ),
+      {
+        accounts: {
+          '0x242B2Dd21e7E1a2b2516d0A3a06b58e2D9BF9196': {
+            dummyProp: 'dummyValue'
+          }
+        }
+      }
     )
   })
 })
