3afd03a589b273b3c0aa1080a83f5921313ae01e
Luca Del Bianco <vshjxyz@gmail.com>
2018-06-01 15:34:42 +0200

    Vaults epics coverage ✅

--- packages/dapp/src/epics/vaults/vaults.js
+++ packages/dapp/src/epics/vaults/vaults.js
@@ -2,34 +2,37 @@ import 'rxjs/add/operator/do'
 import 'rxjs/add/operator/filter'
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/mergeMap'
+import { Scheduler } from 'rxjs/Scheduler'
 import { actionTypes } from '../../constants/action-types'
 import { blockLabels } from '../../constants/blockchain'
+import { fromPromise } from 'rxjs/observable/fromPromise'
 import { merge } from 'rxjs/observable/merge'
 import api from '../../api'
 import vaultActions from '../../actions/vault-actions'
 
-export const vaultsEpic = action$ => {
+export const vaultsEpic = (action$, store, ts = Scheduler.async) => {
   const vaultBlock$ = action$.filter(
     action =>
       action.type === actionTypes.REGISTER_BLOCK &&
       action.payload.label === blockLabels.VAULT
   )
-  const action$1 = vaultBlock$.map(action => {
-    return vaultActions.addRawVault(
-      action.payload.account,
-      action.payload.block
-    )
-  })
+  const action$1 = vaultBlock$.map(action =>
+    vaultActions.addRawVault(action.payload.account, action.payload.block)
+  )
   const action$2 = vaultBlock$
-    .mergeMap(async action => {
+    .mergeMap(action => {
       const address = action.payload.block.args.vault
-      const data = await api.contract.DragoRegistry.fromAddress(address)
       const account = action.payload.account
-      return { account, address, data }
+      return fromPromise(api.contract.DragoRegistry.fromAddress(address)).map(
+        vaultData => ({ account, address, vaultData })
+      )
     })
     .map(
-      // eslint-disable-next-line no-unused-vars
-      ({ account, address, data: [id, name, symbol, _, owner, group] }) => ({
+      ({
+        account,
+        address,
+        vaultData: [id, name, symbol, , owner, group]
+      }) => ({
         account,
         vault: {
           [address]: {
--- packages/dapp/src/epics/vaults/vaults.test.js
+++ packages/dapp/src/epics/vaults/vaults.test.js
@@ -1,56 +1,73 @@
 import { ActionsObservable } from 'redux-observable'
 import { BigNumber } from 'bignumber.js'
 import { TestScheduler } from 'rxjs'
+import { blockLabels } from '../../constants/blockchain'
+import { of } from 'rxjs/observable/of'
 import blockChainActions from '../../actions/blockchain-actions'
 import vaultActions from '../../actions/vault-actions'
 
-const apiMock = {
-  contract: {
-    DragoRegistry: {
-      fromAddress: () => [
-        new BigNumber(0),
-        'Rocksolid Vault',
-        'VLT',
-        '0x242B2Dd21e7E1a2b2516d0A3a06b58e2D9BF9196',
-        '0x242B2Dd21e7E1a2b2516d0A3a06b58e2D9BF9196',
-        null
-      ]
-    }
-  }
-}
-
-jest.doMock('../../api', () => apiMock)
-const vaultsEpic = require('./vaults').vaultsEpic
-
 describe('vaults epics', () => {
+  const owner = '0x242B2Dd21e7E1a2b2516d0A3a06b58e2D9BF9196'
   const vaultEvent = {
     address: '0x001',
     args: {
       vault: '0xc1Eba7b6F9f06E4491a499E653878464e40AB70e',
-      from: '0x242B2Dd21e7E1a2b2516d0A3a06b58e2D9BF9196',
+      from: owner,
       to: '0x005',
       amount: '1',
       revenue: '1'
     },
     event: 'BuyVault'
   }
+  const vaultData = [
+    new BigNumber(0),
+    'Rocksolid Vault',
+    'VLT',
+    owner,
+    owner,
+    null
+  ]
+  const apiMock = {
+    contract: {
+      DragoRegistry: {
+        fromAddress: jest.fn()
+      }
+    }
+  }
+  let fromPromiseSpy
+  let vaultsEpic
+
+  beforeEach(() => {
+    fromPromiseSpy = jest.fn().mockReturnValueOnce(of(vaultData))
+
+    jest.resetModules()
+    jest.doMock('../../api', () => apiMock)
+    jest.doMock('rxjs/observable/fromPromise', () => ({
+      fromPromise: fromPromiseSpy
+    }))
+
+    vaultsEpic = require('./vaults').vaultsEpic
+  })
 
   it('emits a addVault action with a vault object on REGISTER_BLOCK', () => {
     const inputValues = {
-      a: blockChainActions.registerBlock(vaultEvent)
+      a: blockChainActions.registerBlock(owner, blockLabels.VAULT, vaultEvent)
     }
     const expectedValues = {
-      b: vaultActions.addVault({
-        address: '0xc1Eba7b6F9f06E4491a499E653878464e40AB70e',
-        id: 0,
-        name: 'Rocksolid Vault',
-        symbol: 'VLT',
-        owner: '0x242B2Dd21e7E1a2b2516d0A3a06b58e2D9BF9196'
+      b: vaultActions.addRawVault(owner, vaultEvent),
+      c: vaultActions.addVault(owner, {
+        ['0xc1Eba7b6F9f06E4491a499E653878464e40AB70e']: {
+          id: 0,
+          group: null,
+          name: 'Rocksolid Vault',
+          symbol: 'VLT',
+          owner
+        }
       })
     }
 
     const inputMarble = 'a'
-    const expectedMarble = 'b'
+    const expectedMarble = '(bc)'
 
     const ts = new TestScheduler((actual, expected) => {
       expect(actual).toEqual(expected)
@@ -62,8 +79,6 @@ describe('vaults epics', () => {
     const outputAction = vaultsEpic(action$)
 
     ts.expectObservable(outputAction).toBe(expectedMarble, expectedValues)
-
-    ts.maxFrames = 2000
 
     ts.flush()
   })
