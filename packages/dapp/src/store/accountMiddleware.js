53b90fe135eccf57265cb217afc2540ccf80fc80
Federico <federico.obialero@gmail.com>
2018-06-14 16:32:02 +0200

    fix to accountMiddleware

--- packages/dapp/src/store/accountMiddleware.js
+++ packages/dapp/src/store/accountMiddleware.js
@@ -1,4 +1,12 @@
+import actionVariables from '../constants/actions'
+
 export default ({ getState }) => next => action => {
+  const regexp = new RegExp(
+    '^' + actionVariables.persist + '|^' + actionVariables.form
+  )
+  if (regexp.test(action.type)) {
+    return next(action)
+  }
   const account = getState().user.preferences.currentAccount
   action.meta = { currentAccount: account }
   return next(action)
