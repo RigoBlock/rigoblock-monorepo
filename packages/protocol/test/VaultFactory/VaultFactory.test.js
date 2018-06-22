26ac60afbd672839bfa7ee7e9d74019691faf489
Federico Obialero <federico.obialero@gmail.com>
2018-06-22 15:02:16 +0200

    Add tests to RigoToken contracts (#213)
    * Basic testing for VaultFactory
    
    * Added createVault describe
    
    * added RigoToken unit tests, still missing one function tho
    
    * first RigoToken contract tests
    
    * added more tests to proofOfPerformance
    
    * added Inflation tests
    
    * fixed tests now running in sequence so that we can launch them all at the same time
    
    * removed redundant logger
    
    * re-ordered rigotoken tests, now all tests roll back any changes they have done after they finish
    
    * added comments for clarity and minor formatting
    
    * add circleci step, fix memory leak issues caused by pino
    
    * minor fixes
    
    * rename describeContracts to describeContract
    
    * Refactor tests after review
    
    * Using Inflation address as the default DAO address
    
    * Minor refactor
    
    * Add TODO

---
+++ packages/protocol/test/VaultFactory/VaultFactory.test.js
@@ -0,0 +1,46 @@
+const contractName = 'VaultFactory'
+
+describeContract(contractName, () => {
+  describe('createVault', () => {
+    it('creates a vault when provided with proper parameters', async () => {
+      const txHash = await baseContracts[contractName].createVault(
+        'test name',
+        'NAM'
+      )
+      expect(txHash).toBeHash()
+    })
+
+    it('throws an error when provided with the same name', async () => {
+      await baseContracts[contractName].createVault('same name', 'TE1')
+
+      await expect(
+        baseContracts[contractName].createVault('same name', 'TE2')
+      ).rejects.toThrowErrorMatchingSnapshot()
+    })
+
+    it('does not throw an error when provided with the same symbol', async () => {
+      const txHash1 = await baseContracts[contractName].createVault(
+        'some name',
+        'TTT'
+      )
+      const txHash2 = await baseContracts[contractName].createVault(
+        'other name',
+        'TTT'
+      )
+      expect(txHash1).toBeHash()
+      expect(txHash2).toBeHash()
+    })
+
+    it('throws an exception when provided with a symbol longer than 3 characters', async () => {
+      await expect(
+        baseContracts[contractName].createVault('long one', 'TOOLONG')
+      ).rejects.toThrowErrorMatchingSnapshot()
+    })
+
+    it('throws an exception when provided with a symbol longer smaller than 3 characters', async () => {
+      await expect(
+        baseContracts[contractName].createVault('small one', 'SM')
+      ).rejects.toThrowErrorMatchingSnapshot()
+    })
+  })
+})
---
+++ packages/protocol/test/setupTests.js
@@ -0,0 +1,66 @@
+import { GANACHE_NETWORK_ID, GANACHE_PORT, NETWORKS } from '../constants'
+import c from 'chalk'
+import deploy from '@rigoblock/protocol/deploy'
+import ganache from 'ganache-cli'
+import logger from '../deploy/logger'
+import pkg from '../package.json'
+import web3 from './web3'
+
+let server
+process.on('warning', e => console.error(e.stack))
+process.on('error', e => console.error(e.stack))
+
+const setupGanache = async () => {
+  const ganacheOptions = {
+    mnemonic: pkg.config.mnemonic,
+    port: GANACHE_PORT,
+    network_id: GANACHE_NETWORK_ID
+  }
+  server = ganache.server(ganacheOptions)
+  server.listen(
+    GANACHE_PORT,
+    err =>
+      err ? logger.error(err) : logger.info(c.bold.green('Ganache starting!'))
+  )
+  const rawAccounts = await web3.eth.getAccounts()
+  global.accounts = rawAccounts.map(acc => acc.toLowerCase())
+  const prevLog = console.log
+  console.log = () => {}
+  global.baseContracts = await deploy(accounts[0], NETWORKS[0])
+  console.log = prevLog
+}
+
+global.describeContract = (name, f) => {
+  describe('', () => {
+    beforeAll(setupGanache)
+    describe(name, f)
+    afterAll(async () => {
+      const closeGanachePromise = new Promise((resolve, reject) => {
+        server.close(
+          err =>
+            err
+              ? reject(new Error(err))
+              : resolve(logger.info(c.bold.yellow('Ganache stopping...')))
+        )
+      })
+      return await closeGanachePromise
+    })
+  })
+}
+
+expect.extend({
+  toBeHash(received) {
+    try {
+      if (received.substr(0, 2) === '0x' && received.length === 66) {
+        return {
+          message: () => `expected ${received} to be a valid Hash`,
+          pass: true
+        }
+      }
+    } catch (err) {}
+    return {
+      message: () => `expected ${received} to be a valid Hash`,
+      pass: false
+    }
+  }
+})
