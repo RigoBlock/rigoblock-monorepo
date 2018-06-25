7e6a40ea78446d0c62569b4e8eb33c47f91ff6cb
Federico Obialero <federico.obialero@gmail.com>
2018-06-25 18:44:15 +0200

    Title Atom (#230)
    * added Title component, added new font size variables
    
    * added missing stories
    
    * accountView now uses Title component

--- packages/dapp/src/components/organisms/LeftNavbar/LeftNavbar.stories.js
+++ packages/dapp/src/components/organisms/LeftNavbar/LeftNavbar.stories.js
@@ -1,6 +1,50 @@
 import '../../_settings/_base.scss'
+import * as ROUTES from '../../../constants/routes'
+import { MemoryRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { selectV2, withKnobs } from '@storybook/addon-knobs/react'
 import { storiesOf } from '@storybook/react'
 import LeftNavbar from './LeftNavbar'
 import React from 'react'
 
-storiesOf('Organisms/LeftNavbar', module).add('default', () => <LeftNavbar />)
+const mockStore = {
+  getState: () => ({
+    user: {
+      blockChain: {
+        accounts: {
+          '0x242b2dd21e7e1a2b2516d0a3a06b58e2d9bf9196': {
+            provider: 'metamask'
+          },
+          '0x7328ef1d7ab7583eb9968b2f4a9c900f8a2e2d6d': {
+            provider: 'metamask'
+          },
+          '0x8bb7481495d45ccd5cffae1c3a84155fea85a323': {
+            provider: 'metamask'
+          },
+          '0xe198d98b76c529886affb5a74be8b435624bd310': {
+            provider: 'metamask'
+          },
+          '0x927aa991a628aab6851a890fb790a4a7cd0ec446': {
+            provider: 'metamask'
+          }
+        }
+      },
+      preferences: {
+        currentAccount: '0x242B2Dd21e7E1a2b2516d0A3a06b58e2D9BF9196'
+      }
+    },
+    routing: {
+      location: {
+        pathname: selectV2('url', ROUTES, ROUTES.DASHBOARD)
+      }
+    }
+  }),
+  dispatch: () => null,
+  subscribe: () => null
+}
+
+storiesOf('Organisms/LeftNavbar', module)
+  .addDecorator(withKnobs)
+  .addDecorator(story => <MemoryRouter>{story()}</MemoryRouter>)
+  .addDecorator(story => <Provider store={mockStore}>{story()}</Provider>)
+  .add('default', () => <LeftNavbar />)
---
+++ packages/dapp/src/components/organisms/VaultList/VaultList.stories.js
@@ -0,0 +1,47 @@
+import '../../_settings/_base.scss'
+import { Provider } from 'react-redux'
+import { storiesOf } from '@storybook/react'
+import { withKnobs } from '@storybook/addon-knobs/react'
+import React from 'react'
+import VaultList from './VaultList'
+
+const mockStore = {
+  getState: () => ({
+    user: {
+      preferences: {
+        currentAccount: '0x242B2Dd21e7E1a2b2516d0A3a06b58e2D9BF9196'
+      },
+      blockChain: {
+        accounts: {
+          '0x242B2Dd21e7E1a2b2516d0A3a06b58e2D9BF9196': {
+            vaults: {
+              '0x86a1ba4d485ce346bded508e2426798f825558be': {
+                id: 0,
+                name: 'First Vault',
+                symbol: 'asd',
+                owner: '0x242b2dd21e7e1a2b2516d0a3a06b58e2d9bf9196',
+                group: '0x7ce6e371085cb611fb46d5065397223ef2f952ff',
+                totalSupply: 14
+              },
+              '0x421e1cef6e85e78da2470e54af64a626f45afb85': {
+                id: 2,
+                name: 'Third Vault',
+                symbol: 'das',
+                owner: '0x7328ef1d7ab7583eb9968b2f4a9c900f8a2e2d6d',
+                group: '0x7ce6e371085cb611fb46d5065397223ef2f952ff',
+                totalSupply: 20.23498
+              }
+            }
+          }
+        }
+      }
+    }
+  }),
+  dispatch: () => null,
+  subscribe: () => null
+}
+
+storiesOf('Organisms/VaultList', module)
+  .addDecorator(withKnobs)
+  .addDecorator(story => <Provider store={mockStore}>{story()}</Provider>)
+  .add('default', () => <VaultList />)
