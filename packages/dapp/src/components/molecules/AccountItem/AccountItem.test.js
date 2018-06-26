3b83a628dd641d88fa37fb8ea678332f46eae507
Federico <federico.obialero@gmail.com>
2018-06-26 17:59:27 +0200

    fixes to scss, modified list component to accept a divider

--- packages/dapp/src/components/molecules/AccountItem/AccountItem.test.js
+++ packages/dapp/src/components/molecules/AccountItem/AccountItem.test.js
@@ -1,16 +1,25 @@
 import { shallow } from 'enzyme'
-import AccountItem from './AccountItem'
+import AccountItem, { ACCOUNT_ITEM_TYPES } from './AccountItem'
 import toJson from 'enzyme-to-json'
 
-const props = {
+const shortVersionProps = {
   provider: 'metamask',
   number: '0x7328ef1d7ab7583eb9968b2f4a9c900f8a2e2d6d'
 }
+const fullVersionProps = {
+  ...shortVersionProps,
+  appearance: ACCOUNT_ITEM_TYPES.FULL
+}
 
 describe('AccountItem component', () => {
-  it('renders correctly', () => {
+  it('renders correctly (short version)', () => {
     expect(
-      toJson(shallow(createComponentWithProps(AccountItem, props)))
+      toJson(shallow(createComponentWithProps(AccountItem, shortVersionProps)))
+    ).toMatchSnapshot()
+  })
+  it('renders correctly (full version)', () => {
+    expect(
+      toJson(shallow(createComponentWithProps(AccountItem, fullVersionProps)))
     ).toMatchSnapshot()
   })
 })
