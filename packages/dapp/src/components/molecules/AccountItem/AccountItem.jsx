588e37d80ca2d03cefa6f7f6f627cfb81e117756
Federico <federico.obialero@gmail.com>
2018-06-26 16:26:05 +0200

    modified accountItem component to be more reusable

--- packages/dapp/src/components/molecules/AccountItem/AccountItem.jsx
+++ packages/dapp/src/components/molecules/AccountItem/AccountItem.jsx
@@ -3,30 +3,57 @@ import { METAMASK } from '../../../constants/user'
 import MetaMaskLogo from '../../../images/metamask-fox.svg'
 import PropTypes from 'prop-types'
 import React from 'react'
+import Tooltip, { TOOLTIP_SIZES } from '../../atoms/Tooltip'
 
-const AccountItem = ({ provider, number }) => {
-  const displayedAccount = `**** ${number.substr(number.length - 4, 4)}`
+export const ACCOUNT_ITEM_TYPES = {
+  FULL: 'full',
+  SHORT: 'short'
+}
+
+const AccountItem = ({ provider, number, appearance }) => {
+  const hiddenAccount = `**** ${number.substr(number.length - 4, 4)}`
   let displayedProvider
   let logoComponent
+
   if (provider.toLowerCase() === METAMASK.toLowerCase()) {
     displayedProvider = METAMASK
     logoComponent = <MetaMaskLogo />
   }
   // conditional logic for Parity to be added
-  return (
-    <div className="account-item">
+  return appearance === ACCOUNT_ITEM_TYPES.SHORT ? (
+    <div className="account-item-short">
       <div className="logo">{logoComponent}</div>
       <div className="info">
         <span className="provider">{displayedProvider}</span>
-        <span className="account-number">{displayedAccount}</span>
+        <span className="account-number">{hiddenAccount}</span>
       </div>
+    </div>
+  ) : (
+    <div className="account-item-full">
+      <div className="provider">
+        <div className="logo">{logoComponent}</div>
+        <span className="name">{displayedProvider}</span>
+      </div>
+      <span className="account-number">
+        <span>{number}</span>
+        {/* TODO: add copy to clipboard function */}
+        <Tooltip type="filter_none" size={TOOLTIP_SIZES.SMALL} />
+      </span>
     </div>
   )
 }
 
 AccountItem.propTypes = {
   provider: PropTypes.string.isRequired,
-  number: PropTypes.string.isRequired
+  number: PropTypes.string.isRequired,
+  appearance: PropTypes.oneOf([
+    ACCOUNT_ITEM_TYPES.FULL,
+    ACCOUNT_ITEM_TYPES.SHORT
+  ])
+}
+
+AccountItem.defaultProps = {
+  appearance: ACCOUNT_ITEM_TYPES.SHORT
 }
 
 export default AccountItem
