52e309ccd9507af129e6ce52d00f3bc6d952b1d2
Luca Del Bianco <vshjxyz@gmail.com>
2018-07-02 16:38:14 +0200

    Using getNetwork to get the network id (#244)

--- packages/api/src/api.ts
+++ packages/api/src/api.ts
@@ -22,6 +22,12 @@ class Api {
   public web3: Web3Wrapper
   public engine: ProviderEngineFix
 
+  async startEngine() {
+    return new Promise((resolve, reject) => {
+      this.engine.start(err => (err ? reject(err) : resolve()))
+    })
+  }
+
   async init(web3: Web3 = window.web3) {
     this.engine = new ProviderEngine()
     this.engine.addProvider(new InjectedWeb3Subprovider(web3.currentProvider))
@@ -32,17 +38,19 @@ class Api {
     )
 
     this.web3 = new Web3Wrapper(this.engine)
-    const networkId = web3.version.network
+
+    const networkId = await new Promise((resolve, reject) =>
+      web3.version.getNetwork(
+        (err, networkId) => (err ? reject(err) : resolve(networkId))
+      )
+    )
+
     const contractsMap: Contract.ContractsMap = await protocol(networkId)
     const contracts = new Contract()
     await contracts.init(web3, contractsMap)
     this.contract = contracts
 
-    const startEnginePromise = new Promise((resolve, reject) => {
-      this.engine.start(err => (err ? reject(err) : resolve()))
-    })
-
-    await startEnginePromise
+    await this.startEngine()
 
     return this
   }
