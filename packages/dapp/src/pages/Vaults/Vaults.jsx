b94609e2babaf93a2c217d0a12c64e36d4530d95
Federico <federico.obialero@gmail.com>
2018-07-11 15:54:33 +0200

    add react-table. add TablePagination component

--- packages/dapp/src/pages/Vaults/Vaults.jsx
+++ packages/dapp/src/pages/Vaults/Vaults.jsx
@@ -1,17 +1,32 @@
+import * as ROUTES from '../../constants/routes'
+import { connect } from 'react-redux'
 import BaseTemplate from '../../components/templates/BaseTemplate'
+import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import VaultSelect from '../../components/organisms/VaultSelect'
+import VaultTransactionsTable from '../../components/organisms/VaultTransactionsTable'
 
 class Vaults extends Component {
   render() {
     // TODO: use the match prop to render the correct Vault
     // depending on url
-    return (
+    return this.props.location === ROUTES.VAULTS ? (
       <BaseTemplate>
         <VaultSelect />
+      </BaseTemplate>
+    ) : (
+      <BaseTemplate>
+        <VaultSelect />
+        <VaultTransactionsTable vaultId="0" />
       </BaseTemplate>
     )
   }
 }
 
-export default Vaults
+Vaults.propTypes = {
+  location: PropTypes.string.isRequired
+}
+
+export default connect(state => ({
+  location: state.routing.location.pathname
+}))(Vaults)
--- packages/dapp/src/pages/Vaults/Vaults.test.js
+++ packages/dapp/src/pages/Vaults/Vaults.test.js
@@ -1,10 +1,20 @@
+import { Provider } from 'react-redux'
 import { shallow } from 'enzyme'
 import React from 'react'
 import Vaults from './Vaults.jsx'
+import mockStore from '../../fixtures/store'
 import toJson from 'enzyme-to-json'
+
+const store = mockStore({ mockFn: jest.fn })
+
+const wrapper = shallow(
+  <Provider store={store}>
+    <Vaults />
+  </Provider>
+)
 
 describe('Vaults page', () => {
   it('renders correctly', () => {
-    expect(toJson(shallow(<Vaults />))).toMatchSnapshot()
+    expect(toJson(wrapper)).toMatchSnapshot()
   })
 })
