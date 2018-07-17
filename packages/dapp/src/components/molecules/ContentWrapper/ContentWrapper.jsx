b04b9b2c15098ae0faf8698efd0cb50d906c4d9c
Federico <federico.obialero@gmail.com>
2018-07-17 18:51:53 +0200

    minor fix to contentWrapper

--- packages/dapp/src/components/molecules/ContentWrapper/ContentWrapper.jsx
+++ packages/dapp/src/components/molecules/ContentWrapper/ContentWrapper.jsx
@@ -3,9 +3,13 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const ContentWrapper = ({ header, children }) => {
-  return (
+  return header ? (
     <div className="content-wrapper">
       <div className="content-wrapper-header">{header}</div>
+      <div className="content-wrapper-content">{children}</div>
+    </div>
+  ) : (
+    <div className="content-wrapper">
       <div className="content-wrapper-content">{children}</div>
     </div>
   )
