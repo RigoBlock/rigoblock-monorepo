bbd271d9a5e254b2e30404d31a80d4a9596b292d
Federico <federico.obialero@gmail.com>
2018-07-19 18:57:42 +0200

    removed textField from drago page

--- packages/dapp/src/pages/Dragos/Dragos.jsx
+++ packages/dapp/src/pages/Dragos/Dragos.jsx
@@ -1,22 +1,11 @@
 import BaseTemplate from '../../components/templates/BaseTemplate'
 import React, { Component } from 'react'
-import TextFieldWithTitle from '../../components/molecules/TextFieldWithTitle'
-
-const props = {
-  tooltip: "Write here the vault's name",
-  title: 'Vault name',
-  fieldName: 'vaultName',
-  fieldProps: {
-    id: 'testTextField'
-  }
-}
 
 class Dragos extends Component {
   render() {
     return (
       <BaseTemplate>
         <h1>Dragos</h1>
-        <TextFieldWithTitle {...props} />
       </BaseTemplate>
     )
   }
