7c11e8181aa261e644dac19d1ec4d0f3f22d55d5
Federico <federico.obialero@gmail.com>
2018-07-19 13:37:25 +0200

    improved feature tests

--- packages/dapp/helpers/web3Puppeteer.js
+++ packages/dapp/helpers/web3Puppeteer.js
@@ -1,13 +1,52 @@
 'use strict'
 const Helper = codecept_helper
-const fs = require('fs')
+const fs = require('fs-extra')
+const ganache = require('ganache-cli')
+const mnemonic = require('../package.json').config.mnemonic
 
 class Web3Puppeteer extends Helper {
+  constructor() {
+    super()
+    this.ganache = null
+  }
+
   async _before() {
+    fs.emptyDirSync('./ganache-db')
+    console.log('Database directory emptied.')
+    fs.copySync('./ganache-backup', './ganache-db')
+    console.log('Ganache backup restored')
+    this.ganache = ganache.server({
+      mnemonic,
+      db_path: './ganache-db',
+      port: 8545,
+      network_id: 5777
+    })
+    this.ganache.listen(
+      8545,
+      err =>
+        err
+          ? console.error('Error occurred during Ganache startup', err)
+          : console.log('Ganache starting!')
+    )
     await this.helpers['Puppeteer'].amOnPage('/login')
     await this.inject()
     await this.helpers['Puppeteer'].page.waitForNavigation()
   }
+
+  async _after() {
+    const closeGanachePromise = new Promise((resolve, reject) => {
+      this.ganache.close(
+        err =>
+          err
+            ? reject(new Error(err))
+            : resolve(console.log('Ganache stopping...'))
+      )
+    })
+    await closeGanachePromise
+    this.ganache = null
+    return
+  }
+
   async inject() {
     const web3Raw = fs.readFileSync('../../node_modules/web3/dist/web3.min.js')
     const page = this.helpers['Puppeteer'].page
--- packages/dapp/test/pages/vaults.js
+++ packages/dapp/test/pages/vaults.js
@@ -23,7 +23,34 @@ module.exports = {
   testVaultSelect() {
     I.cssClick("div.list-item[id='2']")
     I.waitUrlEquals('/vaults/2')
+    I.see('DAS', 'div.title.large')
     I.cssClick("div.list-item[id='0']")
     I.waitUrlEquals('/vaults/0')
+    I.see('ASD', 'div.title.large')
+  },
+
+  async createNewVault(name, symbol) {
+    I.waitNumberOfVisibleElements('div.list-item', 2, 5)
+    await I.createVault(name, symbol)
+    I.waitNumberOfVisibleElements('div.list-item', 3, 10)
+    I.cssClick("div.list-item[id='4']")
+    I.waitUrlEquals('/vaults/4')
+    I.see(symbol, 'div.title.large')
+    I.see('No transactions found.', 'div.table-replacer')
+  },
+
+  async depositToVault() {
+    const vaultId = 2
+    const amount = '32.18'
+    I.cssClick(`div.list-item[id='${vaultId}']`)
+    await I.buyVault(vaultId, amount)
+    I.waitForText('53.52', 5)
+    I.waitForText(amount, 5)
+  },
+
+  assertAccountValue(val) {
+    I.cssClick('div.navigation-view a[href="/"]')
+    I.waitForVisible('div.accounts-panel', 5)
+    I.waitForText(val, 5)
   }
 }
