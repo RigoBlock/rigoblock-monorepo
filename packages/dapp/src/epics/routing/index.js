225a1613e0a5f78c852648486f346b262cdc3480
Federico <federico.obialero@gmail.com>
2018-07-20 16:13:20 +0200

    removed unexistent file

--- packages/dapp/src/epics/routing/index.js
+++ packages/dapp/src/epics/routing/index.js
@@ -1,6 +1,4 @@
 import loginEpic from './login'
 import logoutEpic from './logout'
-import vaultsPageEpic from './vaultsPage'
 
-export default [loginEpic, logoutEpic, vaultsPageEpic]
-// export default [loginEpic, logoutEpic]
+export default [loginEpic, logoutEpic]
