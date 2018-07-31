1c361996e5d40f6a0a390396b49b6ada631b3b2e
Federico <federico.obialero@gmail.com>
2018-07-31 17:13:05 +0200

    using React fragments instead of classless divs to group elements

--- packages/dapp/src/components/templates/BaseTemplate/BaseTemplate.jsx
+++ packages/dapp/src/components/templates/BaseTemplate/BaseTemplate.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import LeftNavbar from '../../organisms/LeftNavbar'
 import Modal from '../../molecules/Modal'
 import PropTypes from 'prop-types'
-import React, { PureComponent } from 'react'
+import React, { Fragment, PureComponent } from 'react'
 import TopNavbar from '../../organisms/TopNavbar'
 import classNames from 'classnames'
 import globalActions from '../../../actions/global-actions'
@@ -13,7 +13,7 @@ class BaseTemplate extends PureComponent {
     const { children, className, modalComponent, dispatch } = this.props
     const contentClass = classNames('page-main-content', className)
     return (
-      <div>
+      <Fragment>
         <TopNavbar />
         <div className="page-content-section">
           <LeftNavbar />
@@ -23,7 +23,7 @@ class BaseTemplate extends PureComponent {
           Component={modalComponent}
           onClickOutside={() => dispatch(globalActions.closeModal())}
         />
-      </div>
+      </Fragment>
     )
   }
 }
