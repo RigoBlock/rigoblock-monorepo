555c2203a403e5098ce2f134142ff2935971bc04
David Fava <dfava80@gmail.com>
2018-07-31 15:59:42 +0100

    Added transport selection  #321

--- packages/exchange-connector/src/index.js
+++ packages/exchange-connector/src/index.js
@@ -1,31 +1,32 @@
 // Copyright 2017 Rigo Investment Sagl.
 // This file is part of RigoBlock.
 
-import {
-  getTickers,
-  getAggregatedOrders
-} from './exchanges'
+import * as http from './exchanges'
 import { SupportedExchanges } from './const'
 import * as FORMAT from './format'
 import rp from 'request-promise'
 import { ERCdEX, Ethfinex } from '../../const';
 
 class Exchange {
-  constructor(exchange, network = '1') {
+  constructor(exchange, network = '1', transport = 'http') {
     if (typeof SupportedExchanges[exchange] === 'undefined') {
       throw new Error('Exchange not supported: ' + exchange)
     }
     if (!SupportedExchanges[exchange].supportedNetworks.includes(network)) {
       throw new Error('Network not supported on this exchange: ' + network)
     }
-    // console.log(exchange, network)
     this._exchange = exchange
     this._network = network
+    this._transport = transport
     this._exchangeProperties = SupportedExchanges[exchange]
+    this._call = {
+      http,
+      ws: 'ws'
+    }
   }
 
   returnResults = (query, formatFunction = (input) => { return input }) => {
-    return rp(query)
+    return rp(query())
       .then(results => {
         console.log(results)
         console.log(formatFunction)
@@ -45,19 +46,24 @@ class Exchange {
     if (!quoteToken) {
       throw new Error('quoteToken needs to be set')
     }
-    switch (this._exchange) {
-      case ERCdEX:
-        return this.returnResults(getAggregatedOrders[this._exchange](this._network, baseToken.address, quoteToken.address), FORMAT.aggregatedOrders[this._exchange])
-      case Ethfinex:
-        return this.returnResults(getAggregatedOrders[this._exchange](this._network, baseToken.symbol, 'ETH'), FORMAT.aggregatedOrders[this._exchange])
-      default:
-        throw new Error('Relay unknown')
-    }
+    return this.returnResults(() => {
+      switch (this._exchange) {
+        case ERCdEX:
+          return this._call[this._transport].getAggregatedOrders[this._exchange](this._network, baseToken.address, quoteToken.address)
+        case Ethfinex:
+          return this._call[this._transport].getAggregatedOrders[this._exchange](this._network, baseToken.symbol, 'ETH')
+        default:
+          throw new Error('Relay unknown')
+      }
+    }, FORMAT.aggregatedOrders[this._exchange]
+    )
   }
 
   getTickers = () => {
     console.log(`Fetching tokens prices from ${this._exchange}`)
-    return this.returnResults(getTickers[this._exchange](this._network, this._exchangeProperties.tickersTokenPairs), FORMAT.tickers[this._exchange])
+    return this.returnResults(() => {
+      return this._call[this._transport].getTickers[this._exchange](this._network, this._exchangeProperties.tickersTokenPairs)
+    }, FORMAT.tickers[this._exchange])
   }
 }
 
