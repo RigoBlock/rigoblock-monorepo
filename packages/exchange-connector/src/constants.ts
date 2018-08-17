82054d9785bacc5101fdd4de96bb9413862d2de9
Federico Obialero <federico.obialero@gmail.com>
2018-08-17 16:44:38 +0200

    Typescript Exchange Connector (#336)
    * #321
    
    * #321 added get aggregate orders function for ERCdEX
    
    * Added Ethfinex aggregated order book #321
    
    * Added transport selection  #321
    
    * removed yarn lock file from exchange package
    
    * linting files
    
    * added web3 1.0, added command to watch tests
    
    * updated babel and plugins
    
    * wip
    
    * wip
    
    * Eslint fixes
    
    * First baby step
    
    * added Ethfinex class, now off to add unit tests
    
    * wip
    
    * fixed Ethfinex class
    
    * wip tests
    
    * add unit tests for Ethfinex
    
    * exchange.test wip
    
    * fixed issue with undefined enum NETWORKS
    
    * wip
    
    * added tests for ExchangeFactory
    
    * removed files that we currently dont need
    
    * rollback changes to api, protocol and dapp
    
    * fixed getTickers function in ethfinex, refactored tests
    
    * removed unneded files
    
    * Ethfinex getCandles REST call (#339)
    
    resolves #338 
    
    #### :notebook: Overview
    - Added `raw.getCandles` to Ethfinex class
    - Added unit test

---
+++ packages/exchange-connector/src/constants.ts
@@ -0,0 +1,19 @@
+export const MAINNET: string = 'mainnet'
+export const ROPSTEN: string = 'ropsten'
+export const KOVAN: string = 'kovan'
+
+export const PRICE_PRECISION: number = 7
+export const AMOUNT_PRECISION: number = 5
+
+export const TOKENS_DATA_ENDPOINT =
+  'https://cdn.rawgit.com/kvhnuke/etherwallet/mercury/app/scripts/tokens/ethTokens.json'
+
+export enum SupportedExchanges {
+  ETHFINEX
+}
+
+export enum NETWORKS {
+  MAINNET = '1',
+  KOVAN = '42',
+  ROPSTEN = '3'
+}
---
+++ packages/exchange-connector/src/exchangeFactory.test.ts
@@ -0,0 +1,22 @@
+import { Ethfinex } from './Exchanges/ethfinex'
+import { NETWORKS, SupportedExchanges } from './constants'
+import exchangeFactory from './exchangeFactory'
+
+describe('Exchange factory class', () => {
+  it('returns a new instance of the selected Exchange class', () => {
+    const exchange = exchangeFactory(SupportedExchanges.ETHFINEX)
+    expect(exchange).toBeInstanceOf(Ethfinex)
+  })
+
+  it('throws an error if we try to instance an unsupported exchange', () => {
+    expect(() =>
+      exchangeFactory('random' as any)
+    ).toThrowErrorMatchingSnapshot()
+  })
+
+  it('throws an error if we try to instance an exchange with an unsupported network', () => {
+    expect(() =>
+      exchangeFactory(SupportedExchanges.ETHFINEX, NETWORKS.ROPSTEN)
+    ).toThrowErrorMatchingSnapshot()
+  })
+})
