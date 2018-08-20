040d508fc50eabed9b065b055a8c64c157965aae
Federico <federico.obialero@gmail.com>
2018-08-20 16:50:55 +0200

    fixed api to expose all static methods

--- packages/api/src/contracts/contractInterface.ts
+++
@@ -1,6 +0,0 @@
-import * as Web3 from 'web3'
-
-export interface IContract {
-  createAndValidate(web3: Web3, contractAddress: string): Promise<any>
-  isDeployed(): boolean
-}
--- packages/api/src/contracts/index.ts
+++ packages/api/src/contracts/index.ts
@@ -19,27 +19,33 @@ import { VaultFactory } from './models/VaultFactory'
 // Custom extension
 import { ContractExtension } from './contract-extension'
 
-// Contract interface to retain static methods of contracts
-import { IContract } from './contractInterface'
+export type EnhancedContract<T> = T & typeof ContractExtension
+
+// typeof Authority & Authority = needed to expose all static methods and
+// instance methods of the Authority contract
 
 export abstract class ContractModels {
-  Authority: Authority & ContractExtension & IContract
-  ERC20: ERC20 & ContractExtension & IContract
-  RigoToken: RigoToken & ContractExtension & IContract
-  Distribution: Distribution & ContractExtension & IContract
-  Inflation: Inflation & ContractExtension & IContract
-  UnlimitedAllowanceToken: UnlimitedAllowanceToken &
-    ContractExtension &
-    IContract
-  Drago: Drago & ContractExtension & IContract
-  Migrations: Migrations & ContractExtension & IContract
-  Vault: Vault & ContractExtension & IContract
-  DragoEventful: DragoEventful & ContractExtension & IContract
-  Owned: Owned & ContractExtension & IContract
-  VaultEventful: VaultEventful & ContractExtension & IContract
-  DragoFactory: DragoFactory & ContractExtension & IContract
-  OwnedUninitialized: OwnedUninitialized & ContractExtension & IContract
-  VaultFactory: VaultFactory & ContractExtension & IContract
-  DragoRegistry: DragoRegistry & ContractExtension & IContract
-  ProofOfPerformance: ProofOfPerformance & ContractExtension & IContract
+  Authority: EnhancedContract<typeof Authority & Authority>
+  ERC20: EnhancedContract<typeof ERC20 & ERC20>
+  RigoToken: EnhancedContract<typeof RigoToken & RigoToken>
+  Distribution: EnhancedContract<typeof Distribution & Distribution>
+  Inflation: EnhancedContract<typeof Inflation & Inflation>
+  UnlimitedAllowanceToken: EnhancedContract<
+    typeof UnlimitedAllowanceToken & UnlimitedAllowanceToken
+  >
+  Drago: EnhancedContract<typeof Drago & Drago>
+  Migrations: EnhancedContract<typeof Migrations & Migrations>
+  Vault: EnhancedContract<typeof Vault & Vault>
+  DragoEventful: EnhancedContract<typeof DragoEventful & DragoEventful>
+  Owned: EnhancedContract<typeof Owned & Owned>
+  VaultEventful: EnhancedContract<typeof VaultEventful & VaultEventful>
+  DragoFactory: EnhancedContract<typeof DragoFactory & DragoFactory>
+  OwnedUninitialized: EnhancedContract<
+    typeof OwnedUninitialized & OwnedUninitialized
+  >
+  VaultFactory: EnhancedContract<typeof VaultFactory & VaultFactory>
+  DragoRegistry: EnhancedContract<typeof DragoRegistry & DragoRegistry>
+  ProofOfPerformance: EnhancedContract<
+    typeof ProofOfPerformance & ProofOfPerformance
+  >
 }
