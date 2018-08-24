5789cc9f648b6c388876298090fe2bdc13e59cd6
Luca Del Bianco <vshjxyz@gmail.com>
2018-08-24 17:25:56 +0200

    Generic web3 instead of web3 1.0 typings (#373)

--- packages/stats/src/tasks/web3ErrorWrapper.ts
+++ packages/stats/src/tasks/web3ErrorWrapper.ts
@@ -1,5 +1,7 @@
-import Web3 from 'web3'
+import * as Web3 from 'web3'
 import { Job } from 'bull'
+
+const anyWeb3: any = Web3
 
 export default task => async (job: Job) => {
   const { web3Provider = {} } = job.data
@@ -7,15 +9,15 @@ export default task => async (job: Job) => {
   if (!Object.keys(web3Provider).length) {
     throw new Error('Web3 provider required for web3 error wrapper')
   }
-  const web3 = new Web3(
-    new Web3.providers.WebsocketProvider(web3Provider)
+  const web3 = new anyWeb3(
+    new anyWeb3.providers.WebsocketProvider(web3Provider)
   )
   try {
     await task.apply(this, [job, web3])
-    await (<any> web3.currentProvider.connection).close()
+    await web3.currentProvider.connection.close()
     return
   } catch (e) {
-    await (<any> web3.currentProvider.connection).close()
+    await web3.currentProvider.connection.close()
     throw e
   }
 }
