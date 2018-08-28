22ff3dc29b835330e0d8264b587660bb909e9cda
Federico <federico.obialero@gmail.com>
2018-08-28 18:51:04 +0200

    added tests for 0xstandard relayer class

--- packages/exchange-connector/src/exchanges/0xStandardRelayerRaw.test.ts
+++ packages/exchange-connector/src/exchanges/0xStandardRelayerRaw.test.ts
@@ -3,24 +3,54 @@ import * as nock from 'nock'
 import StandardRelayerRaw from './0xStandardRelayerRaw'
 import nockBackPromise from '../nockBackPromise'
 
-describe('it works', () => {
-  beforeEach(() => {
+describe('it allows us to perform API calls to exchanges following 0x Standard Relayer API', () => {
+  let exchange
+  beforeAll(() => {
     nock.disableNetConnect()
+    exchange = new StandardRelayerRaw('https://api.ercdex.com/api/standard/1')
   })
 
-  afterEach(() => {
+  afterAll(() => {
     nock.enableNetConnect()
   })
-
-  it('makes a call', async () => {
-    const exchange = new StandardRelayerRaw(
-      'https://api.ercdex.com/api/standard/1'
-    )
-    const result = await nockBackPromise('getTokenPairs.json', () =>
-      exchange.getTokenPairs()
-    )
-
-    // const result = await exchange.getTokenPairs()
-    console.log(result)
+  describe('getTokenPairs', () => {
+    it('Retrieves a list of available token pairs and the information required to trade them.', async () => {
+      const result: any = await nockBackPromise(
+        '0xStandardRelayerGetTokenPairs.json',
+        () => exchange.getTokenPairs()
+      )
+      expect(result).toMatchSnapshot()
+    })
+  })
+  describe('getOrders', () => {
+    it('Retrieves a list of orders given query parameters.', async () => {
+      const result: any = await nockBackPromise(
+        '0xStandardRelayerGetOrders.json',
+        () => exchange.getOrders()
+      )
+      expect(result).toMatchSnapshot()
+    })
+  })
+  describe('getOrder', () => {
+    it('retrieves a specific order by orderHash.', async () => {
+      const orderHash =
+        '0x7109947a8f4c595f2604445cc4cfc0927fcef5b84b9b33a34528ada629dbd846'
+      const result: any = await nockBackPromise(
+        '0xStandardRelayerGetOrder.json',
+        () => exchange.getOrder(orderHash)
+      )
+      expect(result).toMatchSnapshot()
+    })
+  })
+  describe('getOrderbook', () => {
+    const baseTokenAddress = '0xe41d2489571d322189246dafa5ebde1f4699f498'
+    const quoteTokenAddress = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+    it('retrieves the orderbook for a given token pair.', async () => {
+      const result: any = await nockBackPromise(
+        '0xStandardRelayerGetOrderbook.json',
+        () => exchange.getOrderbook(baseTokenAddress, quoteTokenAddress)
+      )
+      expect(result).toMatchSnapshot()
+    })
   })
 })
--- packages/exchange-connector/src/exchanges/0xStandardRelayerRaw.ts
+++ packages/exchange-connector/src/exchanges/0xStandardRelayerRaw.ts
@@ -1,4 +1,4 @@
-export class StandardRelayerRaw<T = StandardRelayerRaw.RawOrder[]> {
+class StandardRelayerRaw<T = RawOrder[]> {
   constructor(public STANDARD_API_URL, public transport = 'http') {
     if (!STANDARD_API_URL) {
       throw new Error('API url must be specified.')
@@ -12,13 +12,14 @@ export class StandardRelayerRaw<T = StandardRelayerRaw.RawOrder[]> {
   public async getTokenPairs(
     tokenA?: string,
     tokenB?: string
-  ): Promise<StandardRelayerRaw.TokenPair[]> {
+  ): Promise<TokenPair[]> {
     const url = `${
       this.STANDARD_API_URL
     }/v0/token_pairs?tokenA=${tokenA}&tokenB=${tokenB}`
     return fetch(url).then(r => r.json())
   }
 
+  // !! TODO: Add parameters to query
   public async getOrders(
     exchangeContractAddress?: string, // returns orders created for this exchange address
     tokenAddress?: string, // returns orders where makerTokenAddress or takerTokenAddress is token address,
@@ -28,18 +29,13 @@ export class StandardRelayerRaw<T = StandardRelayerRaw.RawOrder[]> {
     taker?: string, // returns orders where taker is taker address,
     trader?: string, // returns orders where maker or taker is trader address,
     feeRecipient?: string // returns orders where feeRecipient is feeRecipient address
-  ): Promise<StandardRelayerRaw.RawOrder[]> {
-    const url = `${
-      this.STANDARD_API_URL
-    }/v0/orders?exchangeContractAddress=${exchangeContractAddress}&tokenAddress=${tokenAddress}
-        &makerTokenAddress=${makerTokenAddress}&takerTokenAddress=${takerTokenAddress}&maker=${maker}
-        &taker=${taker}&trader=${trader}&feeRecipient=${feeRecipient}`
+  ): Promise<RawOrder[]> {
+    const url = `${this.STANDARD_API_URL}/v0/orders`
+    console.log(url)
     return fetch(url).then(r => r.json())
   }
 
-  public async getOrder(
-    orderHash: string
-  ): Promise<StandardRelayerRaw.RawOrder> {
+  public async getOrder(orderHash: string): Promise<RawOrder> {
     const url = `${this.STANDARD_API_URL}/v0/order/${orderHash}`
     return fetch(url).then(r => r.json())
   }
@@ -47,7 +43,7 @@ export class StandardRelayerRaw<T = StandardRelayerRaw.RawOrder[]> {
   public async getOrderbook(
     baseTokenAddress: string, // address of token designated as the baseToken in the currency pair calculation of price
     quoteTokenAddress: string // address of token designated as the quoteToken in the currency pair calculation of price
-  ): Promise<StandardRelayerRaw.OrderBook> {
+  ): Promise<OrderBook> {
     const url = `${
       this.STANDARD_API_URL
     }/v0/orderbook?baseTokenAddress=${baseTokenAddress}&quoteTokenAddress=${quoteTokenAddress}`
@@ -55,50 +51,48 @@ export class StandardRelayerRaw<T = StandardRelayerRaw.RawOrder[]> {
   }
 }
 
-export namespace StandardRelayerRaw {
-  export type TokenPair = {
-    tokenA: {
-      address: string
-      minAmount: string
-      maxAmount: string
-      precision: number
-    }
-    tokenB: {
-      address: string
-      minAmount: string
-      maxAmount: string
-      precision: number
-    }
+export type TokenPair = {
+  tokenA: {
+    address: string
+    minAmount: string
+    maxAmount: string
+    precision: number
   }
+  tokenB: {
+    address: string
+    minAmount: string
+    maxAmount: string
+    precision: number
+  }
+}
 
-  export type RawOrder = {
-    exchangeContractAddress: string
-    maker: string
-    taker: string
-    makerTokenAddress: string
-    takerTokenAddress: string
-    feeRecipient: string
-    makerTokenAmount: string
-    takerTokenAmount: string
-    makerFee: string
-    takerFee: string
-    expirationUnixTimestampSec: string
-    salt: string
-    ecSignature: {
-      v: number
-      r: string
-      s: string
-    }
-    remainingTakerTokenAmount: string
-    orderHash: string
-    source: string
-    price: string
+export type RawOrder = {
+  exchangeContractAddress: string
+  maker: string
+  taker: string
+  makerTokenAddress: string
+  takerTokenAddress: string
+  feeRecipient: string
+  makerTokenAmount: string
+  takerTokenAmount: string
+  makerFee: string
+  takerFee: string
+  expirationUnixTimestampSec: string
+  salt: string
+  ecSignature: {
+    v: number
+    r: string
+    s: string
   }
+  remainingTakerTokenAmount: string
+  orderHash: string
+  source: string
+  price: string
+}
 
-  export type OrderBook = {
-    bids: RawOrder[]
-    asks: RawOrder[]
-  }
+export type OrderBook = {
+  bids: RawOrder[]
+  asks: RawOrder[]
 }
 
 export default StandardRelayerRaw
