34d1fdbf1cf9f5a4df4030e335a3944fd81f4dc9
Federico Obialero <federico.obialero@gmail.com>
2018-08-29 20:50:55 +0200

    Add raw ERCdEX class returning non formatted responses (#383)
    * added ercdexRaw and unit tests, reorganized nock fixtures
    
    * added more unit tests

---
+++ packages/exchange-connector/src/exchanges/ercdexRaw.test.ts
@@ -0,0 +1,101 @@
+import 'jest'
+import 'whatwg-fetch'
+import * as nock from 'nock'
+import { ERCdEXRaw } from './ercdexRaw'
+import { NETWORKS } from '../constants'
+import nockBackPromise from '../nockBackPromise'
+
+describe('it allows us to perform API calls to exchanges following 0x Standard Relayer API', () => {
+  let exchange
+  beforeAll(() => {
+    nock.disableNetConnect()
+    exchange = new ERCdEXRaw(NETWORKS.MAINNET)
+  })
+
+  afterAll(() => {
+    nock.enableNetConnect()
+  })
+  describe('getBestOrders', () => {
+    it('Gets the order(s) representing the best market price', async () => {
+      const result: any = await nockBackPromise(
+        'ercdexRaw/getBestOrders.json',
+        () =>
+          exchange.getBestOrders({
+            makerTokenAddress: '0xe41d2489571d322189246dafa5ebde1f4699f498',
+            takerTokenAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+            baseTokenAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+            quantity: '1',
+            takerAddress: '0x0000000000000000000000000000000000000000'
+          })
+      )
+      expect(result).toMatchSnapshot()
+    })
+    it('Returns an error response if the correct parameters are not passed to the call.', async () => {
+      expect(exchange.getBestOrders()).rejects.toThrowErrorMatchingSnapshot()
+    })
+  })
+  describe('getTickers', () => {
+    it('Retrieves tickers data.', async () => {
+      const result: any = await nockBackPromise(
+        'ercdexRaw/getTickers.json',
+        () => exchange.getTickers()
+      )
+      expect(result).toMatchSnapshot()
+    })
+  })
+  describe('getTradeHistoryLogs', () => {
+    it('Retrieves trade history logs.', async () => {
+      const result: any = await nockBackPromise(
+        'ercdexRaw/getTradeHistoryLogs.json',
+        () => exchange.getTradeHistoryLogs()
+      )
+      expect(result).toMatchSnapshot()
+    })
+  })
+  describe('getHistoricalPrices', () => {
+    it('Gets historical data for order book', async () => {
+      const result: any = await nockBackPromise(
+        'ercdexRaw/getHistoricalPrices.json',
+        () =>
+          exchange.getHistoricalPrices({
+            baseTokenAddress: '0xe41d2489571d322189246dafa5ebde1f4699f498',
+            quoteTokenAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+            startDate: '2018-08-28T10:06:02Z'
+          })
+      )
+      expect(result).toMatchSnapshot()
+    })
+    it('Returns an error response if the correct parameters are not passed to the call.', async () => {
+      expect(
+        exchange.getHistoricalPrices()
+      ).rejects.toThrowErrorMatchingSnapshot()
+    })
+  })
+  describe('getAggregatedOrders', () => {
+    it('Gets historical data for order book', async () => {
+      const result: any = await nockBackPromise(
+        'ercdexRaw/getAggregatedOrders.json',
+        () =>
+          exchange.getAggregatedOrders({
+            baseTokenAddress: '0xe41d2489571d322189246dafa5ebde1f4699f498',
+            quoteTokenAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+          })
+      )
+      expect(result).toMatchSnapshot()
+    })
+    it('Returns an error response if the correct parameters are not passed to the call.', async () => {
+      expect(
+        exchange.getAggregatedOrders()
+      ).rejects.toThrowErrorMatchingSnapshot()
+    })
+  })
+  describe('Standard Relayer calls', () => {
+    it('Works with inherited standard relayer calls', async () => {
+      const result: any = await nockBackPromise(
+        'ercdexRaw/standard-getTokenPairs.json',
+        () => exchange.getTokenPairs()
+      )
+      expect(result).toMatchSnapshot()
+    })
+  })
+})
--- packages/exchange-connector/src/utils.ts
+++ packages/exchange-connector/src/utils.ts
@@ -1,6 +1,6 @@
 export const getQueryParameters = options =>
-  Object.keys(options).reduce((acc, next) => {
-    if (!next) {
+  Object.keys(options).reduce((acc, next, index) => {
+    if (index === 0) {
       return acc + `${next}=${options[next]}`
     }
     return acc + `&${next}=${options[next]}`
