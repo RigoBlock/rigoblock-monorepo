b89e0db5090f7b32737064aa90759629a19e5afc
David Fava <dfava80@gmail.com>
2018-09-09 12:03:20 +0200

    Improved readme. Added react component example.

--- packages/stats/README.md
+++ packages/stats/README.md
@@ -1,8 +1,26 @@
 # Rigoblock Stats
-TODO
 
-## Build docker image (from within rigoblock-monorepo root)
+## Tools
+
+### Chronograf
+
+Chronograf is an open-source web application written in Go and React.js that provides the tools to visualize your monitoring data and easily create alerting and automation rules: https://github.com/influxdata/chronograf
+
+Rigoblock:
+
+http://dev-04.endpoint.network:8888/
+
+### Arena
+
+Rigoblock:
+
+http://dev-04.endpoint.network:4567/
+
+
+## Setup
+
+### Build docker image (from within rigoblock-monorepo root)
 `docker-compose -f ./packages/stats/docker-compose.prod.yml build --build-arg NPM_KEY=<insert npm key> bull`
 
-## Run the whole stack
+### Run the whole stack
 `docker-compose -f ./packages/stats/docker-compose.prod.yml up`
---
+++ packages/stats/examples/graphs/src/linePlot.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import './App.css';
+import Plot from 'react-plotly.js';
+import axios from 'axios'
+
+class LinePlot extends Component {
+  componentDidMount() {
+    this.interval = setInterval(this.refreshGraph, 3000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  refreshGraph = async () => {
+    const response = await axios.get('http://dev-04.endpoint.network:8086/query?pretty=true', {
+      params: {
+        db: 'telegraf',
+        q: `SELECT mean("value") AS "mean_value" FROM "telegraf"."autogen"."${this.props.metric}" WHERE time > now() - 5m AND time < now() AND "id"='${this.props.dragoId}' GROUP BY time(500ms) FILL(previous)`
+      }
+    })
+
+    if (response.data.results && response.data.results.length) {
+      this.setState({
+        data: response.data.results[0].series
+      })
+    }
+  }
+
+  render() {
+    const data = this.state && this.state.data && this.state.data.length? this.state.data[0].values : []
+    const filteredData = data.filter(d => d[1] !== null)
+    const x = filteredData.map(d => d[0])
+    const y = filteredData.map(d => d[1])
+    return (
+        <Plot
+        data={[
+          {
+            x,
+            y,
+            type: 'scatter',
+            mode: 'lines',
+            marker: {color: this.props.color},
+          },
+        ]}
+        layout={ {width: 1024, height: 400, title: `Balance for drago ${this.props.dragoId}`} }
+      />
+    );
+  }
+}
+
+LinePlot.defaultProps = {
+  dragoId: '',
+  metric: '',
+  color: 'red'
+}
+
+export default LinePlot;
