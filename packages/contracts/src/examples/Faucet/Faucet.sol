4ef32ca6a3cd40f893144a6f3a454f510a042ace
Unknown <gabriele@rigoinvestment.com>
2018-09-19 17:11:06 +0200

    fix style
    modified faucet style according to our code standard
    modified directories as per our latest smart contracts folder reorganizations

--- packages/contracts/src/examples/Faucet/Faucet.sol
+++ packages/contracts/src/examples/Faucet/Faucet.sol
@@ -1,43 +1,52 @@
-pragma solidity ^0.4.24;
+/*
+
+ Copyright 2017-2018 RigoBlock, Rigo Investment Sagl.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+*/
 
 /**
- * @title Faucet v0.0.1
  * Faucet smart contract for RigoBlock protocol
  * allows users to receive erc20Basic tokens
  * Inspired by https://github.com/AvocadoNetwork
- * @author David Fava - <https://github.com/wnz99>
  */
 
-import { ERC20Face as Token } from "../utils/tokens/ERC20/ERC20Face.sol";
-import { Owned as Owned } from "../utils/Owned/Owned.sol";
+pragma solidity ^0.4.24;
 
+import { ERC20Face as Token } from "../../tokens/ERC20/ERC20Face.sol";
+import { Owned as Owned } from "../../utils/Owned/Owned.sol";
+
+/// @title Faucet - Allows to automatically distribute GRGs.
+/// @author David Fava - <david@rigoblock.com>
 contract Faucet is Owned {
 
-    /*
-    * Events
-    */
+    // EVENTS
+
     event Deposit(address indexed sender, uint256 value);
     event OneTokenSent(address indexed receiver);
     event FaucetOn(bool status);
     event FaucetOff(bool status);
 
-    /*
-    * Constants
-    */
     uint256 constant oneToken = 1000000000000000000;
     uint256 constant twentyFourHours = 24 hours;
-
-    /*
-    * Storage
-    */
     string public faucetName;
     Token public tokenInstance;
     bool public faucetStatus;
     mapping(address => uint256) status;
 
-    /*
-    * Modifiers
-    */
+    // MODIFIERS
+
     modifier faucetOn() {
         require(faucetStatus,"Faucet has to be on");
         _;
@@ -48,9 +57,6 @@ contract Faucet is Owned {
         _;
     }
 
-    /*
-     * Public functions
-     */
     /// @dev Contract constructor
     /// @param _tokenInstance address of ERC20Basic token
     /// @param _faucetName sets the name for the faucet
@@ -64,21 +70,13 @@ contract Faucet is Owned {
         emit FaucetOn(faucetStatus);
     }
 
-    /// @dev Fallback function allows to deposit ether.
-    // function()
-    //   external
-    //   payable
-    // {
-    //     if (msg.value > 0) {
-    //         emit Deposit(msg.sender, msg.value);
-    //     }
-    // }
+    // CORE FUNCTIONS
 
     /// @dev send 1000 Token with a minimum time lock of 1 hour
     /// @return bool on success
     function drip1Token()
       external
-      faucetOn()
+      faucetOn
       returns (bool success)
     {
         require(!checkStatus(msg.sender),"Required too early.");
@@ -93,7 +91,7 @@ contract Faucet is Owned {
     function turnFaucetOn()
       external
       onlyOwner
-      faucetOff()
+      faucetOff
       returns (bool success)
     {
         faucetStatus = true;
@@ -106,7 +104,7 @@ contract Faucet is Owned {
     function turnFaucetOff()
       external
       onlyOwner
-      faucetOn()
+      faucetOn
       returns (bool success)
     {
         faucetStatus = false;
@@ -114,9 +112,8 @@ contract Faucet is Owned {
         return true;
     }
 
-    /*
-    * Internal functions
-    */
+    // INTERNAL FUNCTIONS
+
     /// @dev locks and unlocks account based on time range
     /// @param _address of msg.sender
     /// @return bool of current lock status of address
@@ -152,6 +149,4 @@ contract Faucet is Owned {
         status[_address] = block.timestamp + _timelock;
         return true;
     }
-
 }
- 
