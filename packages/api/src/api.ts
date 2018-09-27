f145f698a3e25590f11275a188d7275d4ea33485
Federico Obialero <federico.obialero@gmail.com>
2018-09-27 18:58:01 +0200

    Fix API build process (#453)
    * initial commit 🌅
    
    * minor fixes
    
    * removed unused dependencies
    
    * restored build-api circleci step
    
    * fix to package.json
    
    * minor fixes
    
    * removed extra web3 instance
    
    * fixed build script

--- packages/api/src/api.ts
+++ packages/api/src/api.ts
@@ -1,10 +1,9 @@
+import Web3 = require('web3')
 import ProviderEngine = require('web3-provider-engine')
 import * as Contract from './contracts/contract'
 import * as RpcSubprovider from 'web3-provider-engine/subproviders/rpc.js'
-import * as Web3 from 'web3'
 import { ContractModels } from './contracts'
-import { InjectedWeb3Subprovider } from '@0xproject/subproviders'
-import { Web3Wrapper } from '@0xproject/web3-wrapper'
+import { SignerSubprovider } from '@0xproject/subproviders'
 import fetchContracts from '@rigoblock/contracts'
 
 interface Web3Window extends Window {
@@ -13,14 +12,10 @@ interface Web3Window extends Window {
 
 declare let window: Web3Window
 
-// Fix until 0x types are in sync with provider engine
-export interface ProviderEngineFix extends ProviderEngine {
-  start(cb?: Function): void
-}
 class Api {
   public contract: ContractModels
-  public web3: Web3Wrapper
-  public engine: ProviderEngineFix
+  public web3: Web3
+  public engine: ProviderEngine
 
   async startEngine() {
     return new Promise((resolve, reject) => {
@@ -28,29 +23,22 @@ class Api {
     })
   }
 
-  async init(web3: Web3 = window.web3) {
+  async init(web3: Web3 = window.web3, rpcUrl = 'http://localhost:8545') {
     this.engine = new ProviderEngine()
-    this.engine.addProvider(new InjectedWeb3Subprovider(web3.currentProvider))
+    this.engine.addProvider(new SignerSubprovider(<any>web3.currentProvider))
     this.engine.addProvider(
       new RpcSubprovider({
-        rpcUrl: 'http://localhost:8545'
+        rpcUrl
       })
     )
-
-    this.web3 = new Web3Wrapper(this.engine)
-
-    const networkId = await new Promise((resolve, reject) =>
-      web3.version.getNetwork(
-        (err, networkId) => (err ? reject(err) : resolve(networkId))
-      )
-    )
-
+    this.web3 = new Web3(this.engine)
+    const networkId = await this.web3.eth.net.getId()
     const contractsMap: Contract.ContractsMap = await fetchContracts(networkId)
     const contracts = new Contract()
-    await contracts.init(web3, contractsMap)
+    await contracts.init(contractsMap)
     this.contract = contracts
 
-    await this.startEngine()
+    await this.startEngine().catch(err => console.error(err))
 
     return this
   }
