3d8c93740d8c0a63e8d412f59564508f851d6abf
Federico Obialero <federico.obialero@gmail.com>
2018-10-05 17:54:51 +0200

    Feature/create vaults fix (#477)
    * initial commit 🌅
    
    * Initial Template (#450)
    
    * initial commit
    
    * added abi-gen script
    
    * wip
    
    * first tests
    
    * working constant methods
    
    * first contract tests
    
    * template cleanup
    
    * minor fix to tests
    
    * removed abis folder
    
    * removed typechain contracts
    
    * commented non working circleci steps
    
    * removed useless files
    
    * rollback RigoToken test
    
    * eslint ignoring generated files
    
    * minor rollback
    
    * fixes to package.json
    
    * Events Support (#451)
    
    * added getPastEvents method
    
    * added allEvents function
    
    * added once, missing test
    
    * modified event partial
    
    * fixed event partial
    
    * now only contracts with events extend the baseContract
    
    * fixed unit tests
    
    * fixed return types of txFunctions
    
    * removed test file, minor refactor
    
    * Fix API build process (#453)
    
    * initial commit 🌅
    
    * minor fixes
    
    * removed unused dependencies
    
    * restored build-api circleci step
    
    * fix to package.json
    
    * minor fixes
    
    * removed extra web3 instance
    
    * fixed build script
    
    * Update DApp with new API (#454)
    
    * initial commit 🌅
    
    * wip
    
    * wip
    
    * seems to be working
    
    * removed ganache-cli dependency from api
    
    * added contractFactory function
    
    * refactoring and fixed errors
    
    * restored build-dapp step in circleci config
    
    * fixed stuck getId on API file
    
    * refactor
    
    * Fixed DApp unit tests (#462)
    
    * updated babel and dependencies
    
    * fixed blockchainservice tests
    
    * fixed getVaultFees and getVaultSupply
    
    * fixed getVaultTransactions
    
    * fixed selectFirstVault and createVault epic
    
    * updated snapshots temporarily
    
    * fixed remaining unit tests
    
    * removed babel-preset-react-app
    
    * removed unused dependency. fixed babel dependency versions to avoid issues in the future
    
    * fixed circleCI steps
    
    * fix to babel-core version
    
    * refactored code to avoid bugs
    
    * fixed unit and feature tests
    
    * using rawgit url with cdn
    
    * fix to vaults feature test
    
    * code refactor (#466)
    
    * updated createVault to estimate the gas amount rather than using hardcoded one

--- packages/dapp/src/epics/vaults/createVault.js
+++ packages/dapp/src/epics/vaults/createVault.js
@@ -3,11 +3,13 @@ import 'rxjs/add/operator/filter'
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/mergeMap'
 import 'rxjs/add/operator/switchMap'
+import { BigNumber } from 'bignumber.js'
 import { CREATE_VAULT } from '../../constants/transaction-types'
 import { Scheduler } from 'rxjs/Scheduler'
 import { fromPromise } from 'rxjs/observable/fromPromise'
 import { merge } from 'rxjs/observable/merge'
 import { of } from 'rxjs/observable/of'
+import { zip } from 'rxjs/observable/zip'
 import api from '../../api'
 import blockChainActions from '../../actions/blockchain-actions'
 import contractFactory from '../../contractFactory'
@@ -29,25 +31,38 @@ const createVaultEpic = (action$, store, ts = Scheduler.async) => {
           api.contract.VaultFactory.address
         ),
         ts
-      ).switchMap(vaultFactory =>
-        of(vaultFactory)
-          .mergeMap(() =>
+      )
+        .switchMap(vaultFactory => {
+          return zip(
+            fromPromise(api.web3.eth.getGasPrice(), ts),
             fromPromise(
-              vaultFactory
-                .createVault(vaultName.toLowerCase(), vaultSymbol)
-                .then(obj =>
-                  obj.send({ from: accountNumber, gasPrice: 1, gas: 6654755 })
-                ),
+              vaultFactory.createVault(vaultName.toLowerCase(), vaultSymbol),
               ts
-            ).map(txHash =>
-              blockChainActions.transactionCompleted({
-                type: CREATE_VAULT,
-                txHash
-              })
             )
           )
-          .catch(e => of(blockChainActions.transactionFailed(e.toString())))
-      )
+        })
+        .mergeMap(([gasPrice, txObj]) => {
+          const txOptions = { from: accountNumber }
+          return fromPromise(
+            txObj.estimateGas(txOptions).then(gas => {
+              return txObj.send({
+                ...txOptions,
+                gasPrice,
+                // adding 20% to the gas limit as the estimate isn't
+                // always accurate
+                gas: new BigNumber(gas).times(1.2).toFixed(0)
+              })
+            }),
+            ts
+          )
+        })
+        .map(({ transactionHash }) =>
+          blockChainActions.transactionCompleted({
+            type: CREATE_VAULT,
+            txHash: transactionHash
+          })
+        )
+        .catch(e => of(blockChainActions.transactionFailed(e.toString())))
   )
 
   return merge(action$1, action$2)
