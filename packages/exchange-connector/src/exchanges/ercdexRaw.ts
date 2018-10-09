d728c61bb8d883dd3c2c863163c76542a6efd241
David Fava <dfava80@gmail.com>
2018-10-09 10:51:58 +0100

    Fixed Ethfinex network support. Added build:watch script.

--- packages/exchange-connector/src/exchanges/ercdexRaw.ts
+++ packages/exchange-connector/src/exchanges/ercdexRaw.ts
@@ -4,7 +4,7 @@ import { fetchJSON, getQueryParameters, postJSON } from '../utils'
 import ReconnectingWebSocket from 'reconnecting-websocket'
 
 export class ERCdEXRaw extends ZeroExStandardRelayerRaw {
-  static SUPPORTED_NETWORKS: NETWORKS[] = [NETWORKS.MAINNET, NETWORKS.KOVAN]
+  static SUPPORTED_NETWORKS: NETWORKS[] = [NETWORKS.MAINNET, NETWORKS.ROPSTEN]
   public static API_HTTP_URL = 'https://api.ercdex.com/api'
   public static API_WS_URL = 'wss://api.ercdex.com'
   public API_URL: string
