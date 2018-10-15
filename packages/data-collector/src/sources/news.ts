045b2754bc9a30634cdc9d82268159755efe7ca5
Federico <federico.obialero@gmail.com>
2018-10-15 12:59:40 +0200

    link scraping works

--- packages/data-collector/src/sources/news.ts
+++ packages/data-collector/src/sources/news.ts
@@ -11,32 +11,20 @@ export class TokenNews extends HtmlResource {
       res.json()
     )
     const results = response.results.map(res => {
-      // console.log(res.url)
+      console.log(res.slug)
     })
-    const browser = await launch({
-      // headless: false
-      slowMo: 100
-    })
+    const browser = await launch()
     const page = await browser.newPage()
     await page.setViewport({ width: 1980, height: 1020 })
-    await page.goto(
-      'https://cryptopanic.com/news/3315075/0x-Token-Listed-on-Coinbase-Pro-Exchange',
-      { waitUntil: 'networkidle0' }
+    await page.setUserAgent(
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
     )
-
-    await page.screenshot({ path: './screenshot.jpeg', type: 'jpeg' })
+    await page.goto(
+      'https://cryptopanic.com/news/3315075/0x-Token-Listed-on-Coinbase-Pro-Exchange'
+    )
     const html = await page.content()
-    await page.url()
-    await page.waitFor(2000)
-    await page.screenshot({ path: './screenshot2.jpeg', type: 'jpeg' })
-    // browser.close()
     const $ = this.loadHTML(html)
-    const asd = $('h1.post-title').text()
+    const asd = $('h1.post-title a:nth-child(2)').attr('href')
     console.log(asd)
-    // console.log(results)
-
-    // return {
-    //   news: response.results
-    // }
   }
 }
