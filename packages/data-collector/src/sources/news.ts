c7d068c742ea6e395d67fd61159264b4fb30f8c0
Federico <federico.obialero@gmail.com>
2018-10-15 15:51:14 +0200

    wip

--- packages/data-collector/src/sources/news.ts
+++ packages/data-collector/src/sources/news.ts
@@ -10,21 +10,33 @@ export class TokenNews extends HtmlResource {
     const response = await this.fetch(CRYPTO_NEWS_BASE_URL + symbol).then(res =>
       res.json()
     )
-    const results = response.results.map(res => {
-      console.log(res.slug)
-    })
-    const browser = await launch()
-    const page = await browser.newPage()
-    await page.setViewport({ width: 1980, height: 1020 })
-    await page.setUserAgent(
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
-    )
-    await page.goto(
-      'https://cryptopanic.com/news/3315075/0x-Token-Listed-on-Coinbase-Pro-Exchange'
-    )
-    const html = await page.content()
-    const $ = this.loadHTML(html)
-    const asd = $('h1.post-title a:nth-child(2)').attr('href')
-    console.log(asd)
+    const results = response.results.map(res => res.url)
+    const getUrls = async () => {
+      const urls = []
+      const promise = results
+        .map((url, index) =>
+          launch().then(browser =>
+            browser.newPage().then(async page => {
+              await page.setUserAgent(
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
+              )
+              await page.goto(url)
+              await page.screenshot({ path: `./screenshot${index}.jpeg` })
+              const html = await page.content()
+              const $ = this.loadHTML(html)
+              const sourceUrl = $('h1.post-title a:nth-child(2)').attr('href')
+              urls.push(sourceUrl)
+              await browser.close()
+              await new Promise(resolve => setTimeout(resolve, 1000))
+              return
+            })
+          )
+        )
+        .reduce((acc, curr) => acc.then(() => curr), Promise.resolve())
+      await promise
+      console.log(urls)
+      return urls
+    }
+    await getUrls()
   }
 }
