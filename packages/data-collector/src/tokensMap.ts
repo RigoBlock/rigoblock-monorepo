aba9e0e717a63241ccd5f93a572cdf300d942001
Federico <federico.obialero@gmail.com>
2018-10-16 18:24:29 +0200

    initial commit 🌅

--- packages/data-collector/src/tokensMap.ts
+++ packages/data-collector/src/tokensMap.ts
@@ -5,6 +5,18 @@ export default {
     symbol: 'ZRX',
     overviewUrl: `${TOKEN_MARKET_BASE_URL}/0x`
   },
+  OMG: {
+    symbol: 'OMG',
+    overviewUrl: `${TOKEN_MARKET_BASE_URL}/omise-go`
+  },
+  SAN: {
+    symbol: 'SAN',
+    overviewUrl: `${TOKEN_MARKET_BASE_URL}/santiment`
+  },
+  EDO: {
+    symbol: 'EDO',
+    overviewUrl: `${TOKEN_MARKET_BASE_URL}/eidoo`
+  },
   GNT: {
     symbol: 'GNT',
     overviewUrl: `${TOKEN_MARKET_BASE_URL}/golem`
