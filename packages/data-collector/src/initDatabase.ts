414477dbea6ff907c42f0aaa24b18a6d8ac709c5
Federico Obialero <federico.obialero@gmail.com>
2018-10-19 12:00:11 +0200

    initial commit 🌅 (#516)

--- packages/data-collector/src/initDatabase.ts
+++ packages/data-collector/src/initDatabase.ts
@@ -6,14 +6,25 @@ const initDatabase = async () => {
     views: {
       by_symbol_and_date: {
         map:
-          "function (doc) { const [symbol, date ] = doc._id.split('::') emit([symbol, date], doc)}"
+          "function (doc) { const [symbol, date ] = doc._id.split('::'); emit([symbol, date], doc)}"
       }
     }
+  }
+  const indexDef = {
+    index: {
+      fields: [
+        {
+          date: 'asc'
+        }
+      ]
+    },
+    name: 'tokens-news-date'
   }
   await db.init()
   await db.createDb(INFO_DB)
   await db.createDb(NEWS_DB)
-  await db.upsert(INFO_DB, '_design/news', view)
+  await db.upsert(INFO_DB, '_design/info', view)
+  await db.createIndex(NEWS_DB, indexDef)
 }
 
 initDatabase()
