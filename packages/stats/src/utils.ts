5b1880b99ef2eafaee08ef7aa439eb3907a6dd91
Federico <federico.obialero@gmail.com>
2018-12-07 18:09:01 +0100

    minor fix according to review

--- packages/stats/src/utils.ts
+++ packages/stats/src/utils.ts
@@ -1,29 +1,21 @@
 import { Chunk } from './types'
 import { promisify } from 'util'
 
-const getBlockChunks = async (
-  start,
-  end,
-  chunkSize,
-  web3
-): Promise<Chunk[]> => {
+const getBlockChunks = (start, end, chunkSize = 100000): Chunk[] => {
   const startBlock = start
   const chunks = []
   let endBlock = end
-  if (endBlock === 'latest') {
-    endBlock = await web3.eth.getBlockNumber()
-  }
   for (let i = startBlock - 1; i < endBlock; i += chunkSize) {
     const fromBlock = i + 1
-    const toBlock = i + chunkSize > endBlock ? end : i + chunkSize
+    const toBlock = i + chunkSize > end ? end : i + chunkSize
     chunks.push([fromBlock, toBlock])
   }
   return chunks
 }
 
 export const chunkifyEvents = async (method, eventName, web3) => {
-  const chunkSize = 100000
-  const chunks = await getBlockChunks(0, 'latest', chunkSize, web3)
+  const lastBlock = await web3.eth.getBlockNumber()
+  const chunks = getBlockChunks(0, lastBlock)
   const promises = chunks.map(([fromBlock, toBlock]) => {
     const asyncMethod = promisify(method)
     return asyncMethod(eventName, { fromBlock, toBlock })
