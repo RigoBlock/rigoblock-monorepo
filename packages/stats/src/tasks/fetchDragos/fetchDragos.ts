5747e0efc038614d0cecc354604c120a42ad05bf
Federico Obialero <federico.obialero@gmail.com>
2018-12-07 18:18:08 +0100

    Bugfix: Events fetching (#564)
    * wip
    
    * fixed types
    
    * rollback config
    
    * refactored chunkifyEvents
    
    * added chunkify to utils file
    
    * using promisify
    
    * minor fix according to review

--- packages/stats/src/tasks/fetchDragos/fetchDragos.ts
+++ packages/stats/src/tasks/fetchDragos/fetchDragos.ts
@@ -1,27 +1,24 @@
 import Web3 = require('web3')
+import { EventLog } from '../../types'
+import { chunkifyEvents } from '../../utils'
 import fetchContracts from '@rigoblock/contracts'
 import redis from '../../redis'
 import web3ErrorWrapper from '../web3ErrorWrapper'
-
-type Event = {
-  returnValues: {
-    drago: string
-    dragoId?: string
-  }
-}
 
 const task = async (job, web3: Web3) => {
   const { network } = job.data
   const contractsMap = await fetchContracts(network)
   const { abi, address } = contractsMap.DragoEventful
   const dragoEventful = new web3.eth.Contract(abi, address)
-  const dragoEvents = await new Promise<Event[]>((resolve, reject) =>
-    dragoEventful.getPastEvents(
-      'DragoCreated',
-      { fromBlock: 0, toBlock: 'latest' },
-      (errors, events) => (errors ? reject(errors) : resolve(events))
-    )
+  const eventsPromises = await chunkifyEvents(
+    dragoEventful.getPastEvents.bind(dragoEventful),
+    'DragoCreated',
+    web3
   )
+  const fetchResult: Array<EventLog[]> = await Promise.all(eventsPromises)
+  const dragoEvents = fetchResult
+    .filter(arr => arr.length)
+    .reduce((acc, curr) => [...acc, ...curr], [])
   const dragos = dragoEvents.map(dragoEvent => {
     const { drago, dragoId } = dragoEvent.returnValues
     return redis.hset(`dragos:${network}`, drago, dragoId)
