830bd3e15691eeef63152e682e32fbfaf844ac1e
Federico <federico.obialero@gmail.com>
2018-12-18 18:49:11 +0100

    final draft of the calculateNav task

--- packages/stats/src/tasks/calculateNav/calculateNav.ts
+++ packages/stats/src/tasks/calculateNav/calculateNav.ts
@@ -1,7 +1,7 @@
 import Web3 = require('web3')
-import { CONTRACT_ADDRESSES, EFX_TOKENS_LIST, NETWORKS } from '../../constants'
+import { CONTRACT_ADDRESSES, EFX_TOKENS_LIST } from '../../constants'
+import { NETWORKS, supportedExchanges } from '@rigoblock/exchange-connector'
 import { postJSON, toBn, toUnitAmount } from '../../utils'
-import { supportedExchanges } from '@rigoblock/exchange-connector'
 import exchangeConnector from '../../exchangeConnector'
 import fetchContracts from '@rigoblock/contracts'
 import redis from '../../redis'
@@ -27,7 +27,7 @@ const getTokensAndBalances = async (
     }
   }
   const balancePromises = Object.keys(allTokens).map(async symbol => {
-    const { tokenAddress, wrapperAddress, decimals } = tokenRegistry[symbol]
+    const { tokenAddress, wrapperAddress, decimals } = allTokens[symbol]
     let tokenBalance
     if (!tokenAddress && symbol === 'ETH') {
       tokenBalance = await web3.eth.getBalance(dragoAddress)
@@ -52,11 +52,7 @@ const getTokensAndBalances = async (
       }
     }
   })
-  let tokenBalances = await Promise.all(balancePromises)
-  tokenBalances = tokenBalances.filter(
-    token => !!token.balances.total.toNumber()
-  )
-  return tokenBalances
+  return Promise.all(balancePromises)
 }
 
 const getTradingSymbols = tokensArray =>
@@ -83,10 +79,15 @@ const fetchTokenPrices = async (tokens, networkId) => {
     .filter(val => !!val)
 
   const tickers = await exchange.http.getTickers({ symbols })
+  // for ratelimit or other efx related errors
+  if ((tickers as any)[0] === 'error') {
+    throw new Error((tickers as any)[2])
+  }
+
   return tokenWithSymbols.map(token => {
     const { tradingSymbol } = token
     if (!tradingSymbol) {
-      token.priceEth = 1
+      token.priceEth = toBn(1)
       return token
     }
 
@@ -95,41 +96,32 @@ const fetchTokenPrices = async (tokens, networkId) => {
         return ticker[0] === `t${tradingSymbol}`
       })
       .pop()
-
     if (tradingSymbol === 'ETHUSD') {
       token.priceEth = toBn(1).div(toBn(ticker[7]))
     } else {
-      token.priceEth = toBn(ticker[7])
+      token.priceEth = toBn(ticker[7]).dp(5)
     }
     return token
   })
 }
 
-// Balance * prezzo / decimali
-const calculateWeiAmount = tokens =>
-  tokens.map(token => {
-    const { symbol, priceEth, balances, decimals } = token
-    if (symbol === 'ETH') {
-      token.weiBalance = balances.total
-      return token
-    }
-    token.weiBalance = toUnitAmount(balances.total, decimals)
-      .times(priceEth)
-      .times(1e18)
-    return token
-  })
-
-const getTotalSupply = async contract => {
+const calculateNav = async (tokens, contract) => {
+  const totalWeiAmount = tokens
+    .map(token => {
+      const { priceEth, balances, decimals } = token
+      if (priceEth === 1) {
+        return balances.total
+      }
+      return toUnitAmount(balances.total, decimals)
+        .times(priceEth)
+        .times(1e18)
+    })
+    .reduce((acc, curr) => acc.plus(curr), toBn(0))
   const totalSupply = await contract.methods.totalSupply().call()
-  return toUnitAmount(totalSupply, 6)
-}
-
-const calculateNavInWei = (tokens, totalSupply) => {
-  const totalWeiBalance = tokens.reduce(
-    (acc, curr) => acc.plus(curr.weiBalance),
-    toBn(0)
-  )
-  return totalWeiBalance.div(totalSupply)
+  if (totalSupply === '0') {
+    return toBn(0)
+  }
+  return totalWeiAmount.div(toUnitAmount(totalSupply, 6))
 }
 
 const task = async (job, web3: Web3) => {
@@ -137,52 +129,50 @@ const task = async (job, web3: Web3) => {
   const contractsMap = await fetchContracts(network)
   const pools = await redis.hgetall(`${key}:${network}`)
 
-  const addresses = Object.keys(pools)
   const erc20Abi = contractsMap.ERC20.abi
-  const promises = addresses.map(async address => {
+  const navPromises = Object.keys(pools).map(async address => {
     const poolContract = new web3.eth.Contract(
       contractsMap[poolType].abi,
       address
     )
-    const totalSupply = await getTotalSupply(poolContract)
     const tokensWithBalances = await getTokensAndBalances(
       web3,
       erc20Abi,
       address,
       network
     )
-    console.log(tokensWithBalances)
-    // const tokensWithPrices = await fetchTokenPrices(tokensWithBalances, network)
-    // const tokensWithWeiAmount = await calculateWeiAmount(tokensWithPrices)
-    // const navInWei = await calculateNavInWei(tokensWithWeiAmount, totalSupply)
+    const tokensWithPrices = await fetchTokenPrices(tokensWithBalances, network)
+    const nav = await calculateNav(tokensWithPrices, poolContract)
+    const labels = tokensWithPrices.map(({ symbol, balances, priceEth }) => {
+      const wrapperLabel =
+        symbol === 'WETH'
+          ? ''
+          : `,${symbol}W_Balance=${balances.wrapper.toFixed(5)}`
+      return (
+        `${symbol}_Balance=${balances.token.toFixed(
+          5
+        )},${symbol}_Price=${priceEth.toFixed(5)}` + wrapperLabel
+      )
+    })
+    return {
+      address,
+      nav: nav.toFixed(5),
+      labels: labels.join()
+    }
   })
 
-  // const poolAbi = contractsMap[poolType].abi
-
-  // const navPrices = await Promise.all(
-  //   Object.keys(pools).map(async address => {
-  //     const contract = new web3.eth.Contract(poolAbi, address)
-  //     const poolData = await contract.methods.getData().call()
-  //     const { buyPrice } = poolData
-  //     return {
-  //       address,
-  //       buyPrice
-  //     }
-  //   })
-  // )
-
-  // const navPromises = navPrices.map(
-  //   pool =>
-  //     new Promise((resolve, reject) => {
-  //       statsD.gauge(
-  //         `${poolType}.${pool.address}.price,network=${network},price_type=nav`,
-  //         pool.buyPrice,
-  //         (error, bytes) => (error ? reject(error) : resolve(bytes))
-  //       )
-  //     })
-  // )
-
-  // return Promise.all(navPromises)
+  const navs = await Promise.all(navPromises)
+  const gaugePromises = navs.map(
+    pool =>
+      new Promise((resolve, reject) => {
+        statsD.gauge(
+          `${poolType}.${pool.address}.nav,network=${network},${pool.labels}`,
+          pool.nav,
+          (error, bytes) => (error ? reject(error) : resolve(bytes))
+        )
+      })
+  )
+  return Promise.all(gaugePromises)
 }
 
 export default web3ErrorWrapper(task)
