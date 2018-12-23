4e240dbdd6e07be8e1c14a3cddc762590bdc3607
David Fava <dfava80@gmail.com>
2018-12-23 21:24:22 +0100

    Reorganized output of multi data utility contract.

--- packages/contracts/src/examples/helpers/HGetDragoData.sol
+++ packages/contracts/src/examples/helpers/HGetDragoData.sol
@@ -16,36 +16,102 @@
 
 */
 
-pragma solidity 0.4.25;
+pragma solidity 0.5.2;
 pragma experimental ABIEncoderV2;
 
-import { DragoFace } from "../../protocol/Drago/DragoFace.sol";
-import { DragoRegistryFace } from "../../protocol/DragoRegistry/DragoRegistryFace.sol";
+/// @title Drago Interface - Allows interaction with the Drago contract.
+/// @author Gabriele Rigo - <gab@rigoblock.com>
+// solhint-disable-next-line
+interface DragoFace {
+
+    struct Transaction {
+        bytes assembledData;
+    }
+
+    /*
+     * CORE FUNCTIONS
+     */
+    //function() external payable;
+    function buyDrago() external payable returns (bool success);
+    function buyDragoOnBehalf(address _hodler) external payable returns (bool success);
+    function sellDrago(uint256 _amount) external returns (bool success);
+    function setPrices(uint256 _newSellPrice, uint256 _newBuyPrice, uint256 _signaturevaliduntilBlock, bytes32 _hash, bytes calldata _signedData) external;
+    function changeMinPeriod(uint32 _minPeriod) external;
+    function changeRatio(uint256 _ratio) external;
+    function setTransactionFee(uint256 _transactionFee) external;
+    function changeFeeCollector(address _feeCollector) external;
+    function changeDragoDao(address _dragoDao) external;
+    function enforceKyc(bool _enforced, address _kycProvider) external;
+    function setAllowance(address _tokenTransferProxy, address _token, uint256 _amount) external;
+    function setMultipleAllowances(address _tokenTransferProxy, address[] calldata _tokens, uint256[] calldata _amounts) external;
+    function operateOnExchange(address _exchange, Transaction calldata transaction) external returns (bool success);
+    function batchOperateOnExchange(address _exchange, Transaction[] calldata transactions) external;
+
+    /*
+     * CONSTANT PUBLIC FUNCTIONS
+     */
+    function balanceOf(address _who) external view returns (uint256);
+    function getEventful() external view returns (address);
+    function getData() external view returns (string memory name, string memory symbol, uint256 sellPrice, uint256 buyPrice);
+    function calcSharePrice() external view returns (uint256);
+    function getAdminData() external view returns (address, address feeCollector, address dragoDao, uint256 ratio, uint256 transactionFee, uint32 minPeriod);
+    function getKycProvider() external view returns (address);
+    function isValidSignature(bytes32 hash, bytes calldata signature) external view returns (bool isValid);
+    function totalSupply() external view returns (uint256);
+}
+
+/// @title Drago Registry Interface - Allows external interaction with Drago Registry.
+/// @author Gabriele Rigo - <gab@rigoblock.com>
+// solhint-disable-next-line
+interface DragoRegistryFace {
+
+    /*
+     * CORE FUNCTIONS
+     */
+    function register(address _drago, string calldata _name, string calldata _symbol, uint256 _dragoId, address _owner) external payable returns (bool);
+    function unregister(uint256 _id) external;
+    function setMeta(uint256 _id, bytes32 _key, bytes32 _value) external;
+    function addGroup(address _group) external;
+    function setFee(uint256 _fee) external;
+    function updateOwner(uint256 _id) external;
+    function updateOwners(uint256[] calldata _id) external;
+    function upgrade(address _newAddress) external payable; //payable as there is a transfer of value, otherwise opcode might throw an error
+    function setUpgraded(uint256 _version) external;
+    function drain() external;
+
+    /*
+     * CONSTANT PUBLIC FUNCTIONS
+     */
+    function dragoCount() external view returns (uint256);
+    function fromId(uint256 _id) external view returns (address drago, string memory name, string memory symbol, uint256 dragoId, address owner, address group);
+    function fromAddress(address _drago) external view returns (uint256 id, string memory name, string memory symbol, uint256 dragoId, address owner, address group);
+    function fromName(string calldata _name) external view returns (uint256 id, address drago, string memory symbol, uint256 dragoId, address owner, address group);
+    function getNameFromAddress(address _pool) external view returns (string memory);
+    function getSymbolFromAddress(address _pool) external view returns (string memory);
+    function meta(uint256 _id, bytes32 _key) external view returns (bytes32);
+    function getGroups() external view returns (address[] memory);
+    function getFee() external view returns (uint256);
+}
 
 /// @title Drago Data Helper - Allows to query multiple data of a drago at once.
 /// @author Gabriele Rigo - <gab@rigoblock.com>
 // solhint-disable-next-line
 contract HGetDragoData {
-
+    
     struct DragoData {
+        address drago;
         string name;
         string symbol;
         uint256 sellPrice;
         uint256 buyPrice;
-    }
-
-    struct DragoAdminData {
         address owner;
         address feeCollector;
         address dragoDao;
         uint256 ratio;
         uint256 transactionFee;
-        uint32 minPeriod;
-    }
-
-    struct DragoExtraInfo {
         uint256 totalSupply;
         uint256 ethBalance;
+        uint32 minPeriod;
     }
 
     /*
@@ -59,15 +125,11 @@ contract HGetDragoData {
         external
         view
         returns (
-            DragoData memory dragoData,
-            DragoAdminData memory dragoAdminData,
-            DragoExtraInfo memory dragoExtraInfo
+            DragoData memory dragoData
         )
     {
         (
-            dragoData,
-            dragoAdminData,
-            dragoExtraInfo
+            dragoData
         ) = queryDataInternal(_drago);
     }
 
@@ -82,8 +144,6 @@ contract HGetDragoData {
         view
         returns (
             DragoData memory dragoData,
-            DragoAdminData memory dragoAdminData,
-            DragoExtraInfo memory dragoExtraInfo,
             address drago
         )
     {
@@ -91,9 +151,7 @@ contract HGetDragoData {
         DragoRegistryFace dragoRegistryInstance = DragoRegistryFace(dragoRegistry);
         (drago, , , , , ) = dragoRegistryInstance.fromId(_dragoId);
         (
-            dragoData,
-            dragoAdminData,
-            dragoExtraInfo
+            dragoData
         ) = queryDataInternal(drago);
     }
 
@@ -105,23 +163,17 @@ contract HGetDragoData {
         external
         view
         returns (
-            DragoData[] memory,
-            DragoAdminData[] memory,
-            DragoExtraInfo[] memory
+            DragoData[] memory
         )
     {
         uint256 length = _dragoAddresses.length;
         DragoData[] memory dragoData = new DragoData[](length);
-        DragoAdminData[] memory dragoAdminData = new DragoAdminData[](length);
-        DragoExtraInfo[] memory dragoExtraInfo = new DragoExtraInfo[](length);
         for (uint256 i = 0; i < length; i++) {
             (
-                dragoData[i],
-                dragoAdminData[i],
-                dragoExtraInfo[i]
+                dragoData[i]
             ) = queryDataInternal(_dragoAddresses[i]);
         }
-        return(dragoData, dragoAdminData, dragoExtraInfo);
+        return(dragoData);
     }
 
     /// @dev Returns structs of infos on a drago from its ID.
@@ -134,32 +186,46 @@ contract HGetDragoData {
         external
         view
         returns (
-            // DragoData[] memory,
-            // DragoAdminData[] memory,
-            // DragoExtraInfo[] memory,
-            // address drago
-            uint256[] dragoArray;
+            DragoData[] memory
         )
     {
         uint256 length = _dragoIds.length;
-        // DragoArray[] memory dragoArray = new DragoData[](length);
-        // uint256[] dragoArray;
         DragoData[] memory dragoData = new DragoData[](length);
-        DragoAdminData[] memory dragoAdminData = new DragoAdminData[](length);
-        DragoExtraInfo[] memory dragoExtraInfo = new DragoExtraInfo[](length);
+        address[] memory dragos = new address[](length);
         address dragoRegistry = _dragoRegistry;
         DragoRegistryFace dragoRegistryInstance = DragoRegistryFace(dragoRegistry);
         for (uint256 i = 0; i < length; i++) {
             uint256 dragoId = _dragoIds[i];
-            (drago, , , , , ) = dragoRegistryInstance.fromId(dragoId);
+            (dragos[i], , , , , ) = dragoRegistryInstance.fromId(dragoId);
+            dragoData[i].drago = dragos[i];
             (
-                dragoData[i],
-                dragoAdminData[i],
-                dragoExtraInfo[i]
-            ) = queryDataInternal(drago);
-            dragoArray.push(dragoId,drago,dragoData[i], dragoAdminData[i], dragoExtraInfo[i])
+                dragoData[i]
+            ) = queryDataInternal(dragos[i]);
         }
-        return(dragoArray);
+        return(dragoData);
+    }
+
+    /// @dev Returns structs of infos on a drago from its ID.
+    /// @param _dragoAddresses Array of addresses of the target dragos.
+    /// @return Arrays of structs of data and related address of a drago.
+    function queryMultiDataFromAddress(
+        address[] calldata _dragoAddresses)
+        external
+        view
+        returns (
+            DragoData[] memory
+        )
+    {
+        uint256 length = _dragoAddresses.length;
+        DragoData[] memory dragoData = new DragoData[](length);
+        address[] memory dragos = _dragoAddresses;
+        for (uint256 i = 0; i < length; i++) {
+            dragoData[i].drago = dragos[i];
+            (
+                dragoData[i]
+            ) = queryDataInternal(dragos[i]);
+        }
+        return(dragoData);
     }
 
     /*
@@ -173,9 +239,7 @@ contract HGetDragoData {
         internal
         view
         returns (
-            DragoData memory dragoData,
-            DragoAdminData memory dragoAdminData,
-            DragoExtraInfo memory dragoExtraInfo
+            DragoData memory dragoData
         )
     {
         DragoFace dragoInstance = DragoFace(_drago);
@@ -186,14 +250,14 @@ contract HGetDragoData {
             dragoData.buyPrice
         ) = dragoInstance.getData();
         (
-            dragoAdminData.owner,
-            dragoAdminData.feeCollector,
-            dragoAdminData.dragoDao,
-            dragoAdminData.ratio,
-            dragoAdminData.transactionFee,
-            dragoAdminData.minPeriod
+            dragoData.owner,
+            dragoData.feeCollector,
+            dragoData.dragoDao,
+            dragoData.ratio,
+            dragoData.transactionFee,
+            dragoData.minPeriod
         ) = dragoInstance.getAdminData();
-        dragoExtraInfo.totalSupply = dragoInstance.totalSupply();
-        dragoExtraInfo.ethBalance = address(_drago).balance;
+        dragoData.totalSupply = dragoInstance.totalSupply();
+        dragoData.ethBalance = address(_drago).balance;
     }
 }
