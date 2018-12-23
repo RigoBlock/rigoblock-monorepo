72441f20bc007641f0d3a94d1536598a447868d3
David Fava <dfava80@gmail.com>
2018-12-23 21:33:46 +0100

    Changed remote source of solc versions.
    0x deployer fetches solc version from https://cdn.rawgit.com/ethereum/solc-bin/gh-pages/bin/list.json. It's not updated and will also be dismissed in the future. I have changed the url with the link to the official github page.

--- packages/deployer/src/utils/solcVersionFetch.ts
+++ packages/deployer/src/utils/solcVersionFetch.ts
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 export default async () => {
   const solcJson = await fetch(
-    'https://cdn.rawgit.com/ethereum/solc-bin/gh-pages/bin/list.json'
+    'https://raw.githubusercontent.com/ethereum/solc-bin/gh-pages/bin/list.json'
   ).then(res => res.json())
   return solcJson.releases
 }
