e89bd073df07c27cce631c2f21ac99a2aba5a0e0
Luca Del Bianco <vshjxyz@gmail.com>
2018-12-24 13:28:50 +0100

    Further doc gen improvements

--- packages/api/doc-gen.js
+++ packages/api/doc-gen.js
@@ -20,79 +20,97 @@ function capitalize(string) {
 }
 
 const docGen = async networkId => {
-  if (false) {
-    console.log('Renaming readme...')
-    await unlink('./README.md')
-    await copyFile('./README_BASE.md', './README.md')
+  console.log('Renaming readme...')
+  await unlink('./README.md')
+  await copyFile('./README_BASE.md', './README.md')
 
-    // TODO: do this call programmatically as soon
-    // as there's a clear way for TypeDoc to be run from node
-    try {
-      console.log('Launching TypeDoc...')
-      await exec(
-        [
-          'npx typedoc',
-          `--theme markdown`,
-          `--mdEngine github`,
-          `--mdHideSources`,
-          `--out docs src`
-        ].join(' ')
-      )
-    } catch (error) {
-      return console.error(error.stack)
-    }
-
-    console.log('Relinking readme...')
-    await unlink('./README.md')
-    await symlink('./docs/README.md', './README.md')
+  // TODO: do this call programmatically as soon
+  // as there's a clear way for TypeDoc to be run from node
+  try {
+    console.log('Launching TypeDoc...')
+    await exec(
+      [
+        'npx typedoc',
+        `--theme markdown`,
+        `--mdEngine github`,
+        `--mdHideSources`,
+        `--out docs src`
+      ].join(' ')
+    )
+  } catch (error) {
+    return console.error(error.stack)
   }
+
+  console.log('Relinking readme...')
+  await unlink('./README.md')
+  await symlink('./docs/README.md', './README.md')
 
   const contractsMap = await fetchContracts(networkId)
   const docsList = await glob('./docs/**/*.md')
 
+  console.log('Filling descriptions...')
   await Promise.all(
     toPairs(contractsMap).map(async ([contractName, contractObj]) => {
+      console.log(`Filling ${clk.magenta(contractName)}...`)
       const lowCountractName = contractName.toLowerCase()
       const contractDoc = docsList.find(doc => doc.includes(lowCountractName))
       const { methods = {}, title = '' } = contractObj.devDoc
       let docContent = (await readFile(contractDoc)).toString()
+      let tokenizedDoc = docContent.split('\n')
 
-      docContent = docContent.replace(
-        /\n## +Index\n/,
-        `\n${title}\n\n## Index\n`
-      )
+      const indexIx = tokenizedDoc.findIndex(tkn => tkn.match(/## +Index/))
+      tokenizedDoc.splice(indexIx + 1, 0, '')
+      tokenizedDoc.splice(indexIx + 2, 0, title)
 
       docContent = await toPairs(methods).reduce(
-        (prevPromise, [signature, methodObj]) => {
+        (prevPromise, [signature, methodObj]) =>
           prevPromise.then(docContent => {
             // Adding method description
-            const methodName = signature.match(/(.+)\(/)[1]
-            const methodRegexp = new RegExp(`\n### +${methodName}\n`)
-            docContent = docContent.replace(
-              methodRegexp,
-              `\n${capitalize(methodObj.details)}\n\n###  ${methodName}\n`
+            tokenizedDoc = docContent.split('\n')
+            const methodName = signature.match(/^[^\(]*/)[0]
+            const methodIx = tokenizedDoc.findIndex(tkn =>
+              tkn.match(new RegExp(`### +${methodName}`))
             )
+            tokenizedDoc.splice(methodIx + 1, 0, '')
+            tokenizedDoc.splice(methodIx + 2, 0, capitalize(methodObj.details))
 
-            // TODO fix this
-            // Adding header to params table
-            const paramsHeaderRegexp = new RegExp(
-              `(### +${methodName}[\\s\\S]*\\| Name \\| Type \\|)([\\s\\S]*)###`,
-              'm'
+            // Adding parameters description
+            const nextMethodIx = tokenizedDoc.findIndex((tkn, ix) =>
+              ix > methodIx + 2 ? tkn.match(new RegExp(`### +(.+)`)) : false
             )
-            docContent = docContent.replace(
-              paramsHeaderRegexp,
-              `$1 Description |$2###`
-            )
-
-            console.log(docContent)
-          })
-        },
+            let paramTableIx = tokenizedDoc.indexOf('| Name | Type |', methodIx)
+            if (paramTableIx < nextMethodIx) {
+              tokenizedDoc.splice(
+                paramTableIx,
+                1,
+                '| Name | Type | Description |'
+              )
+              tokenizedDoc.splice(
+                paramTableIx + 1,
+                1,
+                '| ------ | ------ | ------ |'
+              )
+              toPairs(methodObj.params || {}).forEach(
+                ([paramName, paramDesc]) => {
+                  const paramIx = tokenizedDoc.findIndex(tkn =>
+                    tkn.match(new RegExp(`\\| ${paramName} \\|.+`))
+                  )
+                  if (paramIx < nextMethodIx) {
+                    tokenizedDoc.splice(
+                      paramIx,
+                      1,
+                      `${tokenizedDoc[paramIx]} ${paramDesc} |`
+                    )
+                  }
+                }
+              )
+            }
+            return tokenizedDoc.join('\n')
+          }),
         Promise.resolve(docContent)
       )
 
-      // await writeFile(contractDoc, docContent)
-      console.log('contract')
-      process.exit(0)
+      await writeFile(contractDoc, docContent)
     })
   )
 }
