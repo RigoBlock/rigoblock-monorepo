413ee9c884978e2d7eb4ddba8716f6b3ca4c5b88
Gabriele Rigo <gabriele.rigo@gmail.com>
2020-06-28 16:41:27 +0200

    add missing solidity dependencies

--- packages/contracts/src/utils/0xUtils/LibMath.sol
+++ packages/contracts/src/utils/0xUtils/LibMath.sol
@@ -18,8 +18,8 @@
 
 pragma solidity ^0.5.9;
 
-import "@0x/contracts-utils/contracts/src/LibSafeMath.sol";
-import "@0x/contracts-utils/contracts/src/LibRichErrors.sol";
+import "./LibSafeMath.sol";
+import "./LibRichErrors.sol";
 import "./LibMathRichErrors.sol";
 
 
