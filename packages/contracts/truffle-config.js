9a118196c55497f663ddd3c9413baa9e1b677439
Gabriele Rigo <gabriele.rigo@gmail.com>
2020-06-28 16:34:21 +0200

    fix truffle configuration
    add dirs and params to truffle-config.js

--- packages/contracts/truffle-config.js
+++ packages/contracts/truffle-config.js
@@ -1,4 +1,24 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  contracts_directory: './src',
+  contracts_build_directory: './artifacts',
+  compilers: {
+    solc: {
+      parser: 'solcjs', // Leverages solc-js purely for speedy parsing
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 1000000
+        }
+      }
+    }
+  },
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*'
+    }
+  }
 }
