7bd01cc065e6da6c68dc2c18c403ac1c9db88713
gab <gabriele.rigo@gmail.com>
2020-07-14 12:50:50 +0200

    add last artifacts

--- packages/contracts/src/protocol/extensions/adapters/ASelfCustody.sol
+++ packages/contracts/src/protocol/extensions/adapters/ASelfCustody.sol
@@ -18,7 +18,22 @@
 
 pragma solidity 0.4.25;
 
-import { SafeMath } from '../../../utils/SafeMath/SafeMath.sol';
+contract SafeMath {
+
+    function safeMul(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+
+    function safeDiv(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b > 0);
+        uint256 c = a / b;
+        assert(a == b * c + a % b);
+        return c;
+    }
+}
+
 
 interface Token {
 
