03d5431957ed4d1ad379ba99900622e26edc9428
gab <gabriele.rigo@gmail.com>
2021-10-12 09:33:03 +0200

    delegate multicalls to self
    - delegate multicalls to self

--- packages/contracts/src/protocol/extensions/adapters/AUniswapV3.sol
+++ packages/contracts/src/protocol/extensions/adapters/AUniswapV3.sol
@@ -168,6 +168,20 @@ contract AUniswapV3 {
         payable
         returns (bytes[] memory results)
     {
-        results = IMulticall(UNISWAP_V3_SWAP_ROUTER_ADDRESS).multicall(data);
+        results = new bytes[](data.length);
+        for (uint256 i = 0; i < data.length; i++) {
+            (bool success, bytes memory result) = address(this).delegatecall(data[i]);
+
+            if (!success) {
+                // Next 5 lines from https://ethereum.stackexchange.com/a/83577
+                if (result.length < 68) revert();
+                assembly {
+                    result := add(result, 0x04)
+                }
+                revert(abi.decode(result, (string)));
+            }
+
+            results[i] = result;
+        }
     }
 }
