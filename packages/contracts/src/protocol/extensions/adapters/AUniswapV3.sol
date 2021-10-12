6b8c69e791ff49b563b9fdd40e6dc97d1f550003
gab <gabriele.rigo@gmail.com>
2021-10-12 09:34:14 +0200

    remove unused import
    - removed unused import from uniswap v3 adapter

--- packages/contracts/src/protocol/extensions/adapters/AUniswapV3.sol
+++ packages/contracts/src/protocol/extensions/adapters/AUniswapV3.sol
@@ -23,7 +23,6 @@ pragma solidity 0.8.7;
 
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/IPeripheryPaymentsWithFee.sol";
-import "@uniswap/v3-periphery/contracts/interfaces/IMulticall.sol";
 
 contract AUniswapV3 {
     
